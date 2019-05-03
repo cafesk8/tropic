522dc53faa49aef8f633dafce4d9420045a4c0d2
Petr Kadlec <petr.kadlec@shopsys.com>
2019-05-03 15:54:07 +0200

    add category settings for show in horizontal menu

---
+++ src/Shopsys/ShopBundle/Migrations/Version20190503153037.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Shopsys\ShopBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20190503153037 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->sql('ALTER TABLE categories ADD show_in_horizontal_menu BOOLEAN NOT NULL DEFAULT FALSE');
+        $this->sql('ALTER TABLE categories ALTER show_in_horizontal_menu DROP DEFAULT');
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+    }
+}
