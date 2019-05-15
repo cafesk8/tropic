d755d72567804202326915ca1cc6473119b548ae
Michal Tomka <michal.tomka@shopsys.com>
2019-05-15 16:02:22 +0200

    create Rest API client
    - Rest api is provided by cURL
    - Rest api provides get, post, delete, put methods (For project Bushman are needed get and post methods only)

---
+++ src/Shopsys/ShopBundle/Component/Rest/Exception/RestException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ShopBundle\Component\Rest\Exception;
+
+interface RestException
+{
+}
---
+++ src/Shopsys/ShopBundle/Component/Rest/RestResponse.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ShopBundle\Component\Rest;
+
+class RestResponse
+{
+    /**
+     * @var int
+     */
+    private $code;
+
+    /**
+     * @var string[]
+     */
+    private $data;
+
+    /**
+     * @param int $code
+     * @param array $data
+     */
+    public function __construct(int $code, array $data)
+    {
+        $this->code = $code;
+        $this->data = $data;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+}
