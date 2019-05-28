c5936901dada961df9db1ae1a26dcfd3d6ed5663
Jan Górecki <jan.gorecki@shopsys.com>
2019-05-28 06:31:43 +0200

    do not visible transport without download pickups

---
+++ src/Shopsys/ShopBundle/Model/Transport/TransportVisibilityCalculation.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ShopBundle\Model\Transport;
+
+use Shopsys\FrameworkBundle\Model\Transport\Transport;
+use Shopsys\FrameworkBundle\Model\Transport\TransportVisibilityCalculation as BaseTransportVisibilityCalculation;
+
+class TransportVisibilityCalculation extends BaseTransportVisibilityCalculation
+{
+    /**
+     * @param \Shopsys\ShopBundle\Model\Transport\Transport $transport
+     * @param \Shopsys\FrameworkBundle\Model\Payment\Payment[] $allPaymentsOnDomain
+     * @param int $domainId
+     * @return bool
+     */
+    public function isVisible(Transport $transport, array $allPaymentsOnDomain, $domainId): bool
+    {
+        if (!$this->independentTransportVisibilityCalculation->isIndependentlyVisible($transport, $domainId)) {
+            return false;
+        }
+
+        if ($transport->isInitialDownload() === true) {
+            return false;
+        }
+
+        return $this->existsIndependentlyVisiblePaymentWithTransport($allPaymentsOnDomain, $transport, $domainId);
+    }
+}
