8052c3707585076aecd0b3f822e0160602aa8312
Jan Górecki <jan.gorecki@shopsys.com>
2019-05-28 16:04:13 +0200

    connecting login form to new design

--- src/Shopsys/ShopBundle/Resources/scripts/frontend/login.js
+++ src/Shopsys/ShopBundle/Resources/scripts/frontend/login.js
@@ -5,41 +5,24 @@
 
     Shopsys.login.Login = function () {
 
-        this.init = function ($loginButton) {
-            $loginButton.click(showWindow);
+        this.init = function ($loginForm) {
+            $loginForm.on('submit', onSubmit);
         };
-
-        function showWindow (event) {
-            Shopsys.ajax({
-                url: $(this).data('url'),
-                type: 'POST',
-                success: function (data) {
-                    var $window = Shopsys.window({
-                        content: data,
-                        textHeading: Shopsys.translator.trans('Login')
-                    });
-
-                    $window.on('submit', '.js-front-login-window', onSubmit);
-                }
-            });
-
-            event.preventDefault();
-        }
 
         function onSubmit () {
             Shopsys.ajax({
-                loaderElement: '.js-front-login-window',
+                loaderElement: '.js-login-box-overlay',
                 type: 'POST',
                 url: $(this).attr('action'),
                 data: $(this).serialize(),
                 success: function (data) {
                     if (data.success === true) {
-                        var $loaderOverlay = Shopsys.loaderOverlay.createLoaderOverlay('.js-front-login-window');
+                        var $loaderOverlay = Shopsys.loaderOverlay.createLoaderOverlay('.js-login-box-overlay');
                         Shopsys.loaderOverlay.showLoaderOverlay($loaderOverlay);
 
                         document.location = data.urlToRedirect;
                     } else {
-                        var $validationErrors = $('.js-window-validation-errors');
+                        var $validationErrors = $('.js-login-validation-errors');
                         if ($validationErrors.hasClass('display-none')) {
                             $validationErrors
                                 .text(Shopsys.translator.trans('This account doesn\'t exist or password is incorrect'))
@@ -55,7 +38,7 @@
     };
 
     Shopsys.register.registerCallback(function ($container) {
-        $container.filterAllNodes('.js-login-button').each(function () {
+        $container.filterAllNodes('.js-front-login-window').each(function () {
             var $login = new Shopsys.login.Login();
             $login.init($(this));
         });
