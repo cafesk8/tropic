14925b2dae5ebc45eeffe2ec323983092294d3ed
Tomáš Gottvald <tomas.gottvald@shopsys.com>
2019-05-30 11:06:47 +0200

    design - WIP - top products list

---
+++ src/Shopsys/ShopBundle/Resources/scripts/frontend/topProductsList.js
@@ -0,0 +1,74 @@
+(function ($) {
+
+    Shopsys = window.Shopsys || {};
+    Shopsys.topProductsList = Shopsys.topProductsList || {};
+
+    Shopsys.topProductsList.init = function ($container) {
+        $container.filterAllNodes('.js-top-products').each(function () {
+            var $currentTopProducts = $(this);
+
+            $currentTopProducts.find('.js-top-products-slides').slick({
+                dots: false,
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                lazyLoad: 'ondemand',
+                mobileFirst: true,
+                infinite: false,
+                prevArrow: $currentTopProducts.filterAllNodes('.js-top-products-prev'),
+                nextArrow: $currentTopProducts.filterAllNodes('.js-top-products-next'),
+                responsive: [
+                    {
+                        breakpoint: Shopsys.responsive.SM,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 680,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: Shopsys.responsive.LG,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 860,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: Shopsys.responsive.VL,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: Shopsys.responsive.XL,
+                        settings: {
+                            slidesToShow: 4,
+                            slidesToScroll: 1
+                        }
+                    }
+                ]
+            });
+        });
+    };
+
+    Shopsys.register.registerCallback(Shopsys.topProductsList.init);
+
+    $(window).resize(function () {
+        Shopsys.timeout.setTimeoutAndClearPrevious('Shopsys.topProductsList.init', Shopsys.topProductsList.init, 200);
+    });
+
+})(jQuery);
