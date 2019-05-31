3bd42ec27b98266b35ba72ec4cacfd934f0537dc
Jan Górecki <jan.gorecki@shopsys.com>
2019-05-31 09:26:24 +0200

    refactor horizontal list

--- src/Shopsys/ShopBundle/Resources/scripts/frontend/horizontalList.js
+++ src/Shopsys/ShopBundle/Resources/scripts/frontend/horizontalList.js
@@ -1,13 +1,23 @@
 (function ($) {
 
     Shopsys = window.Shopsys || {};
-    Shopsys.referencesList = Shopsys.referencesList || {};
+    Shopsys.horizontalList = Shopsys.horizontalList || {};
 
-    Shopsys.referencesList.init = function ($container) {
-        $container.filterAllNodes('.js-references').each(function () {
-            var $currentReferences = $(this);
+    Shopsys.horizontalList.init = function ($container) {
+        $container.filterAllNodes('.js-horizontal-list').each(function () {
+            var $currentGallery = $(this);
+            var galleryType = $currentGallery.data('type');
 
-            $currentReferences.find('.js-references-slides').slick({
+            var types = {
+                'last-visited': [2, 3, 1, 2, 3],
+                'top-products': [1, 2, 3, 3, 4],
+                'products': [2, 3, 2, 3, 6],
+                'references': [1, 2, 3, 3, 4]
+            };
+
+            var selectedType = types[galleryType];
+
+            $currentGallery.find('.js-horizontal-list-slides').slick({
                 dots: false,
                 arrows: true,
                 slidesToShow: 1,
@@ -15,41 +25,41 @@
                 lazyLoad: 'ondemand',
                 mobileFirst: true,
                 infinite: false,
-                prevArrow: $currentReferences.filterAllNodes('.js-references-prev'),
-                nextArrow: $currentReferences.filterAllNodes('.js-references-next'),
+                prevArrow: $currentGallery.filterAllNodes('.js-horizontal-list-action-prev'),
+                nextArrow: $currentGallery.filterAllNodes('.js-horizontal-list-action-next'),
                 responsive: [
                     {
                         breakpoint: Shopsys.responsive.SM,
                         settings: {
-                            slidesToShow: 1,
+                            slidesToShow: selectedType[0],
                             slidesToScroll: 1
                         }
                     },
                     {
                         breakpoint: Shopsys.responsive.MD,
                         settings: {
-                            slidesToShow: 2,
+                            slidesToShow: selectedType[1],
                             slidesToScroll: 1
                         }
                     },
                     {
                         breakpoint: Shopsys.responsive.LG,
                         settings: {
-                            slidesToShow: 3,
+                            slidesToShow: selectedType[2],
                             slidesToScroll: 1
                         }
                     },
                     {
                         breakpoint: Shopsys.responsive.VL,
                         settings: {
-                            slidesToShow: 3,
+                            slidesToShow: selectedType[3],
                             slidesToScroll: 1
                         }
                     },
                     {
                         breakpoint: Shopsys.responsive.XL,
                         settings: {
-                            slidesToShow: 4,
+                            slidesToShow: selectedType[4],
                             slidesToScroll: 1
                         }
                     }
@@ -58,10 +68,10 @@
         });
     };
 
-    Shopsys.register.registerCallback(Shopsys.referencesList.init);
+    Shopsys.register.registerCallback(Shopsys.horizontalList.init);
 
     $(window).resize(function () {
-        Shopsys.timeout.setTimeoutAndClearPrevious('Shopsys.referencesList.init', Shopsys.referencesList.init, 200);
+        Shopsys.timeout.setTimeoutAndClearPrevious('Shopsys.horizontalList.init', Shopsys.horizontalList.init, 200);
     });
 
 })(jQuery);
