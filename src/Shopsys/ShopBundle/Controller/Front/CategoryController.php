315df57dd4a156ef1370c387e29963c8fd00116e
Jan Górecki <jan.gorecki@shopsys.com>
2019-06-05 09:58:40 +0200

    remove unnecessary render category list action

--- src/Shopsys/ShopBundle/Controller/Front/CategoryController.php
+++ src/Shopsys/ShopBundle/Controller/Front/CategoryController.php
@@ -5,7 +5,6 @@ namespace Shopsys\ShopBundle\Controller\Front;
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
 use Shopsys\FrameworkBundle\Model\Category\CategoryFacade;
 use Shopsys\FrameworkBundle\Model\Category\TopCategory\TopCategoryFacade;
-use Shopsys\FrameworkBundle\Model\Customer\CurrentCustomer;
 use Shopsys\ShopBundle\Model\Category\HorizontalCategoryFacade;
 
 class CategoryController extends FrontBaseController
@@ -26,11 +25,6 @@ class CategoryController extends FrontBaseController
     private $topCategoryFacade;
 
     /**
-     * @var \Shopsys\FrameworkBundle\Model\Customer\CurrentCustomer
-     */
-    private $currentCustomer;
-
-    /**
      * @var \Shopsys\ShopBundle\Model\Category\HorizontalCategoryFacade
      */
     private $horizontalCategoryFacade;
@@ -39,20 +33,17 @@ class CategoryController extends FrontBaseController
      * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
      * @param \Shopsys\FrameworkBundle\Model\Category\CategoryFacade $categoryFacade
      * @param \Shopsys\FrameworkBundle\Model\Category\TopCategory\TopCategoryFacade $topCategoryFacade
-     * @param \Shopsys\FrameworkBundle\Model\Customer\CurrentCustomer $currentCustomer
      * @param \Shopsys\ShopBundle\Model\Category\HorizontalCategoryFacade $horizontalCategoryFacade
      */
     public function __construct(
         Domain $domain,
         CategoryFacade $categoryFacade,
         TopCategoryFacade $topCategoryFacade,
-        CurrentCustomer $currentCustomer,
         HorizontalCategoryFacade $horizontalCategoryFacade
     ) {
         $this->domain = $domain;
         $this->categoryFacade = $categoryFacade;
         $this->topCategoryFacade = $topCategoryFacade;
-        $this->currentCustomer = $currentCustomer;
         $this->horizontalCategoryFacade = $horizontalCategoryFacade;
     }
 
@@ -75,28 +66,6 @@ class CategoryController extends FrontBaseController
     {
         return $this->render('@ShopsysShop/Front/Content/Category/top.html.twig', [
             'categories' => $this->topCategoryFacade->getVisibleCategoriesByDomainId($this->domain->getId()),
-        ]);
-    }
-
-    /**
-     * @param \Shopsys\FrameworkBundle\Model\Category\Category[] $categories
-     * @param bool $showProductsCountByCategory
-     */
-    public function categoryListAction(array $categories, $showProductsCountByCategory = true)
-    {
-        if ($showProductsCountByCategory === true) {
-            $pricingGroup = $this->currentCustomer->getPricingGroup();
-            $domainId = $this->domain->getId();
-
-            $listableProductCountsIndexedByCategoryId = $this->categoryFacade
-                ->getListableProductCountsIndexedByCategoryId($categories, $pricingGroup, $domainId);
-        } else {
-            $listableProductCountsIndexedByCategoryId = [];
-        }
-
-        return $this->render('@ShopsysShop/Front/Content/Category/categoryList.html.twig', [
-            'categories' => $categories,
-            'listableProductCountsIndexedByCategoryId' => $listableProductCountsIndexedByCategoryId,
         ]);
     }
 
