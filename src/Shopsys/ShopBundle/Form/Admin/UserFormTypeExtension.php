73db1c1223aadf66bebbce3c134b79e83b7eb783
Michal Tomka <michal.tomka@shopsys.com>
2019-06-18 14:39:01 +0200

    show user data from IS in administration

--- src/Shopsys/ShopBundle/Form/Admin/UserFormTypeExtension.php
+++ src/Shopsys/ShopBundle/Form/Admin/UserFormTypeExtension.php
@@ -3,6 +3,8 @@
 namespace Shopsys\ShopBundle\Form\Admin;
 
 use Shopsys\FrameworkBundle\Form\Admin\Customer\UserFormType;
+use Shopsys\FrameworkBundle\Form\DisplayOnlyType;
+use Shopsys\ShopBundle\Model\Customer\User;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -13,6 +15,21 @@ class UserFormTypeExtension extends AbstractTypeExtension
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $user = $options['user'];
+        /* @var $user \Shopsys\ShopBundle\Model\Customer\User */
+
+        if ($user instanceof User) {
+            $systemDataGroupBuilder = $builder->get('systemData');
+
+            $systemDataGroupBuilder->add('transferId', DisplayOnlyType::class, [
+                'label' => t('ID z IS'),
+                'data' => $user->getTransferId() ?? t('ID nenastaveno'),
+            ])
+            ->add('branchNumber', DisplayOnlyType::class, [
+                'label' => t('EAN věrnostní karty'),
+                'data' => $user->getBranchNumber() ?? t('EAN nenastaven'),
+            ]);
+        }
     }
 
     /**
