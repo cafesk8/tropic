67dcea6cfc6850a0dca2b52c1eedea92e93add7d
Pavel Simek <pavel.simek@shopsys.com>
2019-06-19 15:56:18 +0200

    blog article: add products

---
+++ src/Shopsys/ShopBundle/Migrations/Version20190619113205.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Shopsys\ShopBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20190619113205 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->sql('
+            CREATE TABLE blog_article_products (
+                blog_article_id INT NOT NULL,
+                product_id INT NOT NULL,
+                PRIMARY KEY(blog_article_id, product_id)
+            )');
+        $this->sql('CREATE INDEX IDX_925185D79452A475 ON blog_article_products (blog_article_id)');
+        $this->sql('CREATE INDEX IDX_925185D74584665A ON blog_article_products (product_id)');
+        $this->sql('
+            ALTER TABLE
+                blog_article_products
+            ADD
+                CONSTRAINT FK_925185D79452A475 FOREIGN KEY (blog_article_id) REFERENCES blog_articles (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->sql('
+            ALTER TABLE
+                blog_article_products
+            ADD
+                CONSTRAINT FK_925185D74584665A FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+    }
+}
