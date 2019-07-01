3235c25af002c41bff7d1040cc546e09128bd322
Jan Górecki <jan.gorecki@shopsys.com>
2019-07-01 15:14:26 +0200

    create blog category translations for all locales (error on devel/production) and create friendly urls for all domains (error on sk and de domain because in menu there is link to main category)

--- src/Shopsys/ShopBundle/Migrations/Version20190513153157.php
+++ src/Shopsys/ShopBundle/Migrations/Version20190513153157.php
@@ -141,10 +141,9 @@ class Version20190513153157 extends AbstractMigration
             foreach ($this->getAllDomainIds() as $domainId) {
                 $locale = $this->getDomainLocale($domainId);
 
-                $this->sql('INSERT INTO blog_category_translations (translatable_id, name, locale) VALUES (2, \'Hlavní stránka blogu - ' . $locale . '\', \'cs\')');
+                $this->sql('INSERT INTO blog_category_translations (translatable_id, name, locale) VALUES (2, \'Hlavní stránka blogu - ' . $locale . '\', \'' . $locale . '\')');
+                $this->sql('INSERT INTO friendly_urls (domain_id, slug, route_name, entity_id, main) VALUES (' . $domainId . ', \'blog/\', \'front_blogcategory_detail\', 2, true)');
             }
-
-            $this->sql('INSERT INTO friendly_urls (domain_id, slug, route_name, entity_id, main) VALUES (1, \'blog/\', \'front_blogcategory_detail\', 2, true)');
         }
     }
 
