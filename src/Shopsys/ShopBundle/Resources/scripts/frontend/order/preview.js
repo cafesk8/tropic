b9efad2bb855bfeb448411f88d5659bc999dbeec
Michal Tomka <michal.tomka@shopsys.com>
2019-07-03 15:29:26 +0200

    remove unnecessary attributes

--- src/Shopsys/ShopBundle/Resources/scripts/frontend/order/preview.js
+++ src/Shopsys/ShopBundle/Resources/scripts/frontend/order/preview.js
@@ -13,16 +13,10 @@
         var $orderPreview = $('#js-order-preview');
         var $checkedTransport = $('.js-order-transport-input:checked');
         var $checkedPayment = $('.js-order-payment-input:checked');
-        var $backButton = $('.js-backButton');
-        var $submitButton = $('.js-submitButton');
         var $orderPreviewItemsList = $orderPreview.filterAllNodes('#js-order-preview-fees');
         var $orderPreviewTotalPrice = $orderPreview.filterAllNodes('#js-order-preview-total-price');
 
         var data = {};
-        data['submitButtonText'] = $submitButton.val();
-        data['backButtonText'] = $backButton.html();
-        data['backButtonLink'] = $backButton.attr('href');
-
         if ($checkedTransport.length > 0) {
             data['transportId'] = $checkedTransport.data('id');
         }
