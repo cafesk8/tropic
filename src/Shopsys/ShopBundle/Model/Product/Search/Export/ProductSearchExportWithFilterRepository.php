6891c3fdc9dc19b3218bfd15021cf12feb296b53
Jan Górecki <jan.gorecki@shopsys.com>
2019-07-09 09:27:07 +0200

    export action price to elasticsearch

---
+++ src/Shopsys/ShopBundle/Model/Product/Search/Export/ProductSearchExportWithFilterRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ShopBundle\Model\Product\Search\Export;
+
+use Shopsys\FrameworkBundle\Model\Product\Search\Export\ProductSearchExportWithFilterRepository as BaseProductSearchExportWithFilterRepository;
+
+class ProductSearchExportWithFilterRepository extends BaseProductSearchExportWithFilterRepository
+{
+    /**
+     * @param int $domainId
+     * @param string $locale
+     * @param int $startFrom
+     * @param int $batchSize
+     * @return array
+     */
+    public function getProductsData(int $domainId, string $locale, int $startFrom, int $batchSize): array
+    {
+        $queryBuilder = $this->createQueryBuilder($domainId, $locale)
+            ->setFirstResult($startFrom)
+            ->setMaxResults($batchSize);
+
+        $query = $queryBuilder->getQuery();
+
+        $result = [];
+        /** @var \Shopsys\ShopBundle\Model\Product\Product $product */
+        foreach ($query->getResult() as $product) {
+            $flagIds = $this->extractFlags($product);
+            $categoryIds = $this->extractCategories($domainId, $product);
+            $parameters = $this->extractParameters($locale, $product);
+            $prices = $this->extractPrices($domainId, $product);
+
+            $result[] = [
+                'id' => $product->getId(),
+                'catnum' => $product->getCatnum(),
+                'partno' => $product->getPartno(),
+                'ean' => $product->getEan(),
+                'name' => $product->getName($locale),
+                'description' => $product->getDescription($domainId),
+                'shortDescription' => $product->getShortDescription($domainId),
+                'brand' => $product->getBrand() ? $product->getBrand()->getId() : '',
+                'flags' => $flagIds,
+                'categories' => $categoryIds,
+                'in_stock' => $product->getCalculatedAvailability()->getDispatchTime() === 0,
+                'prices' => $prices,
+                'action_price' => $product->getActionPrice($domainId) ? (float)$product->getActionPrice($domainId)->getAmount() : null,
+                'parameters' => $parameters,
+                'ordering_priority' => $product->getOrderingPriority(),
+                'calculated_selling_denied' => $product->getCalculatedSellingDenied(),
+            ];
+        }
+
+        return $result;
+    }
+}
