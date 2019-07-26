699d783a2b37be86e633a1ec45960f1d31bdaf1d
Tomáš Gottvald <tomas.gottvald@shopsys.com>
2019-07-26 13:08:28 +0200

    design - product list variants slider

---
+++ src/Shopsys/ShopBundle/Resources/scripts/frontend/product/variantsSlider.js
@@ -0,0 +1,36 @@
+(function ($) {
+
+    Shopsys = window.Shopsys || {};
+    Shopsys.variantsSlider = Shopsys.variantsSlider || {};
+
+    Shopsys.variantsSlider.init = function ($container) {
+        $container.filterAllNodes('.js-list-products-item').on("hover", function () {
+            var $currentGallery = $(this);
+            $currentGallery.find('.js-variantsSlider-slides:not(.slick-initialized)').slick({
+                dots: false,
+                arrows: true,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                lazyLoad: 'ondemand',
+                infinite: false,
+                prevArrow: $currentGallery.filterAllNodes('.js-variantsSlider-action-prev'),
+                nextArrow: $currentGallery.filterAllNodes('.js-variantsSlider-action-next')
+            });
+
+            $currentGallery.filterAllNodes('.js-variantsSlider-action-prev').click(function(e){
+                e.preventDefault();
+            });
+            $currentGallery.filterAllNodes('.js-variantsSlider-action-next').click(function(e){
+                e.preventDefault();
+            });
+        });
+
+    };
+
+    Shopsys.register.registerCallback(Shopsys.variantsSlider.init);
+
+    $(window).resize(function () {
+        Shopsys.timeout.setTimeoutAndClearPrevious('Shopsys.variantsSlider.init', Shopsys.variantsSlider.init, 200);
+    });
+
+})(jQuery);
