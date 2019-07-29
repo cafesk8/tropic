0a3e2ef626f7e2672e0a969d371c46b7e1c6f4a2
Tomáš Gottvald <tomas.gottvald@shopsys.com>
2019-07-29 13:54:30 +0200

    design - fix standards

--- src/Shopsys/ShopBundle/Resources/scripts/frontend/todo.js
+++ src/Shopsys/ShopBundle/Resources/scripts/frontend/todo.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
     // toggle promo code im cart preview
-    $('.js-promo-code-toggle-checkbox').on("change", function (event) {
+    $('.js-promo-code-toggle-checkbox').on('change', function (event) {
         $(this).toggleClass('active');
         $('.js-promo-code-toggle-content').toggleClass('active');
     });
