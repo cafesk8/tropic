4d2ff32d89be42474d6bf6a3e28ab512c1d66cba
Michal Tomka <michal.tomka@shopsys.com>
2019-08-06 16:03:42 +0200

    fix standards

--- src/Shopsys/ShopBundle/Component/Image/Exception/MigrateImageToEntityFailedException.php
+++ src/Shopsys/ShopBundle/Component/Image/Exception/MigrateImageToEntityFailedException.php
@@ -12,7 +12,7 @@ class MigrateImageToEntityFailedException extends \Exception
      * @param string $entityClassOrName
      * @param \Exception|null $previous
      */
-    public function __construct($entityClassOrName, Exception $previous = null)
+    public function __construct($entityClassOrName, ?Exception $previous = null)
     {
         parent::__construct('Not found image config for entity "' . $entityClassOrName . '".', 0, $previous);
     }
