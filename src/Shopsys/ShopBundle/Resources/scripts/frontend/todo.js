40ff640f6e871378f196abd2df5b44c5c781ba4b
Tomáš Gottvald <tomas.gottvald@shopsys.com>
2019-08-07 07:42:06 +0200

    design - filter toggler - toggle classes only on click event

--- src/Shopsys/ShopBundle/Resources/scripts/frontend/todo.js
+++ src/Shopsys/ShopBundle/Resources/scripts/frontend/todo.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function () {
     $('.js-product-filter-opener').click(function (e) {
+        $('.js-product-list-panel').toggleClass('active');
+        $('.js-product-filter').toggleClass('active-mobile');
         e.preventDefault();
         setFilterPosition();
     });
@@ -10,12 +12,8 @@ $(document).ready(function () {
         if ($(window).width() < 1024) {
             var position = $('.js-product-list').position();
             var newPosition = position.top;
-            $('.js-product-list-panel').toggleClass('active');
-            $('.js-product-filter').toggleClass('active-mobile');
             $('.js-product-list-panel').css({ 'top': newPosition });
         } else {
-            $('.js-product-list-panel').removeClass('active');
-            $('.js-product-filter').removeClass('active-mobile');
             $('.js-product-list-panel').css({ 'top': 0 });
         }
     };
