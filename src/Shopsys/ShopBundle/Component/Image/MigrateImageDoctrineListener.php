e5c944e6af6f310c16bfaeca8fad861cc71d172e
Michal Tomka <michal.tomka@shopsys.com>
2019-08-08 11:40:40 +0200

    remove MountManager

--- src/Shopsys/ShopBundle/Component/Image/MigrateImageDoctrineListener.php
+++ src/Shopsys/ShopBundle/Component/Image/MigrateImageDoctrineListener.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\Event\LifecycleEventArgs;
 use IOException;
 use League\Flysystem\FileExistsException;
 use League\Flysystem\FilesystemInterface;
-use League\Flysystem\MountManager;
 use Shopsys\FrameworkBundle\Component\FileUpload\FileNamingConvention;
 use Shopsys\FrameworkBundle\Component\Image\Config\ImageConfig;
 use Shopsys\ShopBundle\Component\FileUpload\FileUpload;
@@ -31,11 +30,6 @@ class MigrateImageDoctrineListener
     private $migrateProductImagesDir;
 
     /**
-     * @var \League\Flysystem\MountManager
-     */
-    private $mountManager;
-
-    /**
      * @var \League\Flysystem\FilesystemInterface
      */
     private $filesystem;
@@ -44,20 +38,17 @@ class MigrateImageDoctrineListener
      * @param string $migrateProductImagesDir
      * @param \Shopsys\FrameworkBundle\Component\FileUpload\FileNamingConvention $fileNamingConvention
      * @param \Shopsys\ShopBundle\Component\FileUpload\FileUpload $fileUpload
-     * @param \League\Flysystem\MountManager $mountManager
      * @param \League\Flysystem\FilesystemInterface $filesystem
      */
     public function __construct(
         $migrateProductImagesDir,
         FileNamingConvention $fileNamingConvention,
         FileUpload $fileUpload,
-        MountManager $mountManager,
         FilesystemInterface $filesystem
     ) {
         $this->migrateProductImagesDir = $migrateProductImagesDir;
         $this->fileNamingConvention = $fileNamingConvention;
         $this->fileUpload = $fileUpload;
-        $this->mountManager = $mountManager;
         $this->filesystem = $filesystem;
     }
 
@@ -99,7 +90,6 @@ class MigrateImageDoctrineListener
         $migrateFilename = $image->getMigrateFileName();
         $sourceFilepath = $this->migrateProductImagesDir . '/' . $migrateFilename;
 
-        $targetDirectoryPrefix = $image->getType() !== null ? $image->getType() . '/' : '';
         $targetFilename = $this->fileNamingConvention->getFilenameByNamingConvention(
             FileNamingConvention::TYPE_ID,
             $migrateFilename,
@@ -110,14 +100,14 @@ class MigrateImageDoctrineListener
             $targetFilename,
             true,
             $image->getEntityName(),
-            $targetDirectoryPrefix . ImageConfig::ORIGINAL_SIZE_NAME
+            ImageConfig::ORIGINAL_SIZE_NAME
         );
 
         try {
             if ($this->filesystem->has($targetFilepath)) {
                 $this->filesystem->delete($targetFilepath);
             }
-            $this->mountManager->copy('local://' . $sourceFilepath, 'main://' . $targetFilepath);
+            $this->filesystem->put($targetFilepath, $this->filesystem->read($sourceFilepath));
         } catch (FileExistsException | IOException $ex) {
             $message = sprintf(
                 'Failed to rename file from migrate directory to entity (from `%s` to `%s`), exception: `%s`',
