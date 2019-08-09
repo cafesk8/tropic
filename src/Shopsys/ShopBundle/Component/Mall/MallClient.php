529a19f7df5ff2bea1bd452c88c93747f124e51e
Jan Górecki <jan.gorecki@shopsys.com>
2019-08-09 06:43:36 +0200

    [US-6307] Connection to Mall API

---
+++ src/Shopsys/ShopBundle/Component/Mall/MallClient.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ShopBundle\Component\Mall;
+
+use MPAPI\Services\Client;
+
+class MallClient
+{
+    /**
+     * @var \MPAPI\Services\Client
+     */
+    private $client;
+
+    /**
+     * @param string $apiKey
+     */
+    public function __construct(
+        string $apiKey
+    ) {
+        $this->client = new Client($apiKey);
+    }
+
+    /**
+     * @return \MPAPI\Services\Client
+     */
+    public function getClient(): Client
+    {
+        return $this->client;
+    }
+}
