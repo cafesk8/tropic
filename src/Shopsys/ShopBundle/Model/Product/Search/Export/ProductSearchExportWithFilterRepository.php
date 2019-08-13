f1f9f2737e0cc3fd5d92a583e584ef4d1d0cf718
Jan Górecki <jan.gorecki@shopsys.com>
2019-08-13 10:38:17 +0200

    [BG-5599] Send current date to elastic if selling from is not filled

--- src/Shopsys/ShopBundle/Model/Product/Search/Export/ProductSearchExportWithFilterRepository.php
+++ src/Shopsys/ShopBundle/Model/Product/Search/Export/ProductSearchExportWithFilterRepository.php
@@ -48,7 +48,7 @@ class ProductSearchExportWithFilterRepository extends BaseProductSearchExportWit
                 'parameters' => $parameters,
                 'ordering_priority' => $product->getOrderingPriority(),
                 'calculated_selling_denied' => $product->getCalculatedSellingDenied(),
-                'selling_from' => $product->getSellingFrom()->format('Y-m-d'),
+                'selling_from' => ($product->getSellingFrom() !== null) ? $product->getSellingFrom()->format('Y-m-d') : date('Y-m-d'),
             ];
         }
 
