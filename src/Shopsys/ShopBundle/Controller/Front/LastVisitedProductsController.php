5421f626baacfed1c0be827e5bd6ddc398962be5
Tomas Pesek <tomas.pesek@shopsys.com>
2019-08-19 14:37:30 +0200

    [BG-5649] set sizes on last visited products

--- src/Shopsys/ShopBundle/Controller/Front/LastVisitedProductsController.php
+++ src/Shopsys/ShopBundle/Controller/Front/LastVisitedProductsController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shopsys\ShopBundle\Controller\Front;
 
 use Shopsys\ShopBundle\Model\Product\LastVisitedProducts\LastVisitedProductsFacade;
+use Shopsys\ShopBundle\Model\Product\ProductOnCurrentDomainElasticFacade;
 use Symfony\Component\HttpFoundation\Request;
 
 class LastVisitedProductsController extends FrontBaseController
@@ -17,11 +18,18 @@ class LastVisitedProductsController extends FrontBaseController
     private $lastVisitedProductFacade;
 
     /**
-     * @param \Shopsys\ShopBundle\Model\Product\LastVisitedProducts\LastVisitedProductsFacade $lastVisitedProductFacade
+     * @var \Shopsys\ShopBundle\Model\Product\ProductOnCurrentDomainElasticFacade
      */
-    public function __construct(LastVisitedProductsFacade $lastVisitedProductFacade)
+    private $productOnCurrentDomainFacade;
+
+    /**
+     * @param \Shopsys\ShopBundle\Model\Product\LastVisitedProducts\LastVisitedProductsFacade $lastVisitedProductFacade
+     * @param \Shopsys\ShopBundle\Model\Product\ProductOnCurrentDomainElasticFacade $productOnCurrentDomainFacade
+     */
+    public function __construct(LastVisitedProductsFacade $lastVisitedProductFacade, ProductOnCurrentDomainElasticFacade $productOnCurrentDomainFacade)
     {
         $this->lastVisitedProductFacade = $lastVisitedProductFacade;
+        $this->productOnCurrentDomainFacade = $productOnCurrentDomainFacade;
     }
 
     /**
@@ -37,6 +45,7 @@ class LastVisitedProductsController extends FrontBaseController
 
         return $this->render('@ShopsysShop/Front/Content/LastVisitedProducts/list.html.twig', [
             'products' => $products,
+            'variantsIndexedByMainVariantId' => $this->productOnCurrentDomainFacade->getVariantsIndexedByMainVariantId($products),
         ]);
     }
 }
