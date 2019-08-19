fcec5b62a85e5a3e018fc1531d1ea61a4882f4bb
Tomas Pesek <tomas.pesek@shopsys.com>
2019-08-19 14:34:39 +0200

    [BG-5648] sizes on top products

--- src/Shopsys/ShopBundle/Controller/Front/HomepageController.php
+++ src/Shopsys/ShopBundle/Controller/Front/HomepageController.php
@@ -10,6 +10,7 @@ use Shopsys\FrameworkBundle\Model\Product\TopProduct\TopProductFacade;
 use Shopsys\FrameworkBundle\Model\Seo\SeoSettingFacade;
 use Shopsys\FrameworkBundle\Model\Slider\SliderItemFacade;
 use Shopsys\ShopBundle\Model\Blog\Article\BlogArticleFacade;
+use Shopsys\ShopBundle\Model\Product\ProductOnCurrentDomainElasticFacade;
 
 class HomepageController extends FrontBaseController
 {
@@ -46,12 +47,18 @@ class HomepageController extends FrontBaseController
     private $blogArticleFacade;
 
     /**
+     * @var \Shopsys\ShopBundle\Model\Product\ProductOnCurrentDomainElasticFacade
+     */
+    private $productOnCurrentDomainFacade;
+
+    /**
      * @param \Shopsys\FrameworkBundle\Model\Customer\CurrentCustomer $currentCustomer
      * @param \Shopsys\FrameworkBundle\Model\Slider\SliderItemFacade $sliderItemFacade
      * @param \Shopsys\FrameworkBundle\Model\Product\TopProduct\TopProductFacade $topProductsFacade
      * @param \Shopsys\FrameworkBundle\Model\Seo\SeoSettingFacade $seoSettingFacade
      * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
      * @param \Shopsys\ShopBundle\Model\Blog\Article\BlogArticleFacade $blogArticleFacade
+     * @param \Shopsys\ShopBundle\Model\Product\ProductOnCurrentDomainElasticFacade $productOnCurrentDomainFacade
      */
     public function __construct(
         CurrentCustomer $currentCustomer,
@@ -59,7 +66,8 @@ class HomepageController extends FrontBaseController
         TopProductFacade $topProductsFacade,
         SeoSettingFacade $seoSettingFacade,
         Domain $domain,
-        BlogArticleFacade $blogArticleFacade
+        BlogArticleFacade $blogArticleFacade,
+        ProductOnCurrentDomainElasticFacade $productOnCurrentDomainFacade
     ) {
         $this->currentCustomer = $currentCustomer;
         $this->sliderItemFacade = $sliderItemFacade;
@@ -67,6 +75,7 @@ class HomepageController extends FrontBaseController
         $this->seoSettingFacade = $seoSettingFacade;
         $this->domain = $domain;
         $this->blogArticleFacade = $blogArticleFacade;
+        $this->productOnCurrentDomainFacade = $productOnCurrentDomainFacade;
     }
 
     public function indexAction()
@@ -88,6 +97,7 @@ class HomepageController extends FrontBaseController
                 self::HOMEPAGE_ARTICLES_LIMIT
             ),
             'domainId' => $this->domain->getId(),
+            'variantsIndexedByMainVariantId' => $this->productOnCurrentDomainFacade->getVariantsIndexedByMainVariantId($topProducts),
         ]);
     }
 }
