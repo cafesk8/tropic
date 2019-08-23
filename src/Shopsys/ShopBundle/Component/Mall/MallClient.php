c5c23c06ad138d7ba70060d73e6b5e7d112c2112
Jan Górecki <jan.gorecki@shopsys.com>
2019-08-23 15:41:08 +0200

    do not use mall exception handler

--- src/Shopsys/ShopBundle/Component/Mall/MallClient.php
+++ src/Shopsys/ShopBundle/Component/Mall/MallClient.php
@@ -19,7 +19,7 @@ class MallClient
     public function __construct(
         string $apiKey
     ) {
-        $this->client = new Client($apiKey);
+        $this->client = new Client($apiKey, false);
     }
 
     /**
