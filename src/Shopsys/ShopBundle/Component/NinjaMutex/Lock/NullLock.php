6d30fba867ab40d2072ec0a0424cbc3e074b09bd
Jan Górecki <jan.gorecki@shopsys.com>
2019-08-23 14:44:41 +0200

    Cloud: fix exit code 255 for cron

---
+++ src/Shopsys/ShopBundle/Component/NinjaMutex/Lock/NullLock.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Shopsys\ShopBundle\Component\NinjaMutex\Lock;
+
+use NinjaMutex\Lock\LockAbstract;
+use NinjaMutex\Lock\LockExpirationInterface;
+
+class NullLock extends LockAbstract implements LockExpirationInterface
+{
+    /**
+     * @param int $expiration Expiration time of the lock in seconds. If it's equal to zero (default), the lock will never expire.
+     *                        Max 2592000s (30 days), if greater it will be capped to 2592000 without throwing an error.
+     *                        WARNING: Using value higher than 0 may lead to race conditions. If you set too low expiration time
+     *                        e.g. 30s and critical section will run for 31s another process will gain lock at the same time,
+     *                        leading to unpredicted behaviour. Use with caution.
+     */
+    public function setExpiration($expiration)
+    {
+    }
+
+    /**
+     * Clear lock without releasing it
+     * Do not use this method unless you know what you do
+     *
+     * @param  string $name name of lock
+     * @return bool
+     */
+    public function clearLock($name)
+    {
+        return true;
+    }
+
+    /**
+     * @param  string $name name of lock
+     * @param  bool   $blocking
+     * @return bool
+     */
+    protected function getLock($name, $blocking)
+    {
+        return true;
+    }
+
+    /**
+     * Release lock
+     *
+     * @param  string $name name of lock
+     * @return bool
+     */
+    public function releaseLock($name)
+    {
+        return true;
+    }
+
+    /**
+     * Check if lock is locked
+     *
+     * @param  string $name name of lock
+     * @return bool
+     */
+    public function isLocked($name)
+    {
+        return false;
+    }
+}
