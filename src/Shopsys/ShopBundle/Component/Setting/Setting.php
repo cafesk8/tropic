fa2276f76aaae5c422265c22700c11f8d843f322
Jakub Kadela <jakub.kadela@shopsys.com>
2019-08-26 15:41:29 +0200

    admin can set time of delivery deadline in admin

---
+++ src/Shopsys/ShopBundle/Component/Setting/Setting.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ShopBundle\Component\Setting;
+
+use Shopsys\FrameworkBundle\Component\Setting\Setting as BaseSetting;
+
+class Setting extends BaseSetting
+{
+    public const ORDER_TRANSPORT_DEADLINE_HOURS = 'orderTransportDeadlineHours';
+    public const ORDER_TRANSPORT_DEADLINE_MINUTES = 'orderTransportDeadlineMinutes';
+}
