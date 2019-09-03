8e602c72099504ef1e68aa054c72713811354b6d
Pavel Simek <pavel.simek@shopsys.com>
2019-09-03 13:54:05 +0200

    [BG-5841] process footer links todo.js

---
+++ src/Shopsys/ShopBundle/Resources/scripts/frontend/footerLink.js
@@ -0,0 +1,30 @@
+(function ($) {
+
+    Shopsys = window.Shopsys || {};
+    Shopsys.FooterLink = Shopsys.FooterLink || {};
+
+    Shopsys.FooterLink.init = function() {
+        $footerLinkTitle = $('.js-footer-link-title');
+        var footerLinkWrapperSelector = '.js-footer-link-wrapper';
+        var footerLinkContentSelector = '.js-footer-link-content';
+
+        $footerLinkTitle.unbind('click');
+
+        $firstFooterLinkTitle = $footerLinkTitle.first();
+
+        var isFirstFooterLinkContentVisible = $firstFooterLinkTitle.closest(footerLinkWrapperSelector).find(footerLinkContentSelector).is(':visible');
+
+        if (isFirstFooterLinkContentVisible === false) {
+            $footerLinkTitle.on('click', function () {
+                $(this).toggleClass('open');
+                $(this).closest(footerLinkWrapperSelector).find(footerLinkContentSelector).slideToggle();
+            });
+        }
+    };
+
+    Shopsys.register.registerCallback(Shopsys.FooterLink.init);
+
+    // In case of rotate screen on mobile and tablet
+    $(window).resize(Shopsys.FooterLink.init);
+
+})(jQuery);
