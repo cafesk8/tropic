942cf3523f6f64e20e632433121c86ed9a6aa893
Tomáš Gottvald <tomas.gottvald@shopsys.com>
2019-09-08 19:53:18 +0200

    design - search opener focus to input

---
+++ src/Shopsys/ShopBundle/Resources/scripts/frontend/todo.js
@@ -0,0 +1,25 @@
+/* TODO PRG */
+(function ($) {
+
+    Shopsys = window.Shopsys || {};
+    Shopsys.DropDown = Shopsys.DropDown || {};
+
+    $footerLinkTitle = $('.js-footer-link-title');
+    $footerLinkContent = $('.js-footer-link-content');
+
+    $footerLinkTitle.on('click', function () {
+        $(this).toggleClass('open');
+        $(this).parent().find($footerLinkContent).slideToggle();
+    });
+
+    // search auto focus on popup activation
+    $searchButtonOpener = $('.js-search-opener');
+    $searchButtonOpener.on('mouseover', function () {
+            setTimeout( function() {
+                $searchButtonOpener.find('input[type=text]').focus();
+                console.log('e');
+            }, 500
+        );
+    });
+
+})(jQuery);
