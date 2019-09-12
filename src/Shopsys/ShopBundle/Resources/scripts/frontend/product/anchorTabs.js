43ff5da2cfa46cbf2eb8a1e0604dae499d589e96
Tomas Pesek <tomas.pesek@shopsys.com>
2019-09-12 15:21:48 +0200

    [BG-5915] fix anchor tab

--- src/Shopsys/ShopBundle/Resources/scripts/frontend/product/anchorTabs.js
+++ src/Shopsys/ShopBundle/Resources/scripts/frontend/product/anchorTabs.js
@@ -6,9 +6,13 @@
             e.preventDefault();
             var target = $(this).data('tab-id');
 
-            $('html, body').animate({
-                scrollTop: $('.js-tabs-content[data-tab-id=' + target + ']').offset().top
-            }, 1000);
+            $('.js-tabs-content[data-tab-id=' + target + ']').each(function () {
+                if ($(this).closest('.box-detail').hasClass('display-none') === false) {
+                    $('html, body').animate({
+                        scrollTop: $(this).offset().top
+                    }, 1000);
+                }
+            });
 
             return false;
         });
