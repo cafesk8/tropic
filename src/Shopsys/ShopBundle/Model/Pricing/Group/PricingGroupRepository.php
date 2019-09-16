e464a84b8cdac67dbca8a2290a447e9e9a38552e
Jan Górecki <jan.gorecki@shopsys.com>
2019-09-16 22:04:01 +0200

    correct calculate bushman pricing groups - default pricing group must be saved first

--- src/Shopsys/ShopBundle/Model/Pricing/Group/PricingGroupRepository.php
+++ src/Shopsys/ShopBundle/Model/Pricing/Group/PricingGroupRepository.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace Shopsys\ShopBundle\Model\Pricing\Group;
 
+use Doctrine\ORM\Query;
+use Shopsys\FrameworkBundle\Component\Doctrine\SortableNullsWalker;
 use Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroupRepository as BasePricingGroupRepository;
 
 class PricingGroupRepository extends BasePricingGroupRepository
@@ -33,5 +35,18 @@ class PricingGroupRepository extends BasePricingGroupRepository
     public function findByDiscount(float $discount, int $userDomainId): ?PricingGroup
     {
         return $this->getPricingGroupRepository()->findOneBy(['discount' => $discount, 'domainId' => $userDomainId]);
+    }
+
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroup[]
+     */
+    public function getAll()
+    {
+        return $this->getPricingGroupRepository()
+            ->createQueryBuilder('pg')
+            ->orderBy('pg.discount', 'asc')
+            ->getQuery()
+            ->setHint(Query::HINT_CUSTOM_OUTPUT_WALKER, SortableNullsWalker::class)
+            ->getResult();
     }
 }
