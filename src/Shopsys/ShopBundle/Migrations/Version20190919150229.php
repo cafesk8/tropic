8da6cd317be23cec46516ffc6803cf66c2ac42f6
Pavel Simek <pavel.simek@shopsys.com>
2019-09-19 15:11:32 +0200

    admin of store: set show on store list

---
+++ src/Shopsys/ShopBundle/Migrations/Version20190919150229.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ShopBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20190919150229 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        $this->sql('ALTER TABLE stores ADD show_on_store_list BOOLEAN NOT NULL DEFAULT TRUE');
+        $this->sql('ALTER TABLE stores ALTER show_on_store_list DROP DEFAULT');
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+    }
+}
