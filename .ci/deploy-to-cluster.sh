642eb0551105cbcbf0b75092160a18ad87a16093
Jan Górecki <jan.gorecki@shopsys.com>
2019-09-27 08:40:20 +0200

    [BG-5060] Set hostname for smtp server

--- .ci/deploy-to-cluster.sh
+++ .ci/deploy-to-cluster.sh
@@ -53,6 +53,7 @@ if [ ${RUNNING_PRODUCTION} -eq "1" ]; then
     yq write --inplace kubernetes/ingress.yml spec.tls[0].hosts[+] www.${DOMAIN_HOSTNAME_2}
     yq write --inplace kubernetes/ingress.yml spec.tls[0].hosts[+] www.${DOMAIN_HOSTNAME_3}
     yq write --inplace kubernetes/ingress.yml metadata.annotations."\"nginx.ingress.kubernetes.io/from-to-www-redirect\"" "\"true\""
+    yq write --inplace kubernetes/deployments/smtp-server.yml spec.template.spec.hostname ${DOMAIN_HOSTNAME_1}
 else
     yq write --inplace kubernetes/ingress.yml metadata.annotations."\"nginx.ingress.kubernetes.io/auth-type\"" basic
     yq write --inplace kubernetes/ingress.yml metadata.annotations."\"nginx.ingress.kubernetes.io/auth-secret\"" shopsys
