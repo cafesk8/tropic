6cf1836c9546e73d7a0eb3ff4c86459c639bc4cd
Tomáš Gottvald <tomas.gottvald@shopsys.com>
2019-10-01 11:30:49 +0200

    design - product detail - tabs open/close on mobile

--- src/Shopsys/ShopBundle/Resources/scripts/frontend/product/anchorTabs.js
+++ src/Shopsys/ShopBundle/Resources/scripts/frontend/product/anchorTabs.js
@@ -5,14 +5,19 @@
         $container.filterAllNodes('.js-anchor-tabs .js-tabs-button').click(function (e) {
             e.preventDefault();
             var target = $(this).data('tab-id');
-
-            $('.js-tabs-content[data-tab-id=' + target + ']').each(function () {
-                if ($(this).closest('.box-detail').hasClass('display-none') === false) {
-                    $('html, body').animate({
-                        scrollTop: $(this).offset().top
-                    }, 1000);
-                }
-            });
+            if ($(window).width() > 1024) {
+                $('.js-tabs-content[data-tab-id=' + target + ']').each(function () {
+                    if ($(this).closest('.box-detail').hasClass('display-none') === false) {
+                        $(this).show();
+                        $('html, body').animate({
+                            scrollTop: $(this).offset().top
+                        }, 1000);
+                    }
+                });
+            } else {
+                $(this).toggleClass('active');
+                $('.js-tabs-content[data-tab-id=' + target + ']').slideToggle();
+            }
 
             return false;
         });
