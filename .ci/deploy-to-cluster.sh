878284423dc550bf7288b53e9a0992ceadf128f8
Michal Tomka <michal.tomka@shopsys.com>
2019-10-10 09:44:35 +0200

    [US-6719] GoPay Sk credentials

--- .ci/deploy-to-cluster.sh
+++ .ci/deploy-to-cluster.sh
@@ -184,11 +184,16 @@ yq write --inplace app/config/parameters.yml parameters[gtm.config].sk.container
 yq write --inplace app/config/parameters.yml parameters[gtm.config].de.enabled ${GTM_DE_ENABLED}
 yq write --inplace app/config/parameters.yml parameters[gtm.config].de.container_id ${GTM_DE_CONTAINER_ID}
 
-#GoPay
+#GoPay CS
 yq write --inplace app/config/parameters.yml parameters[gopay.config].cs.goid ${GOPAY_CS_GO_ID}
 yq write --inplace app/config/parameters.yml parameters[gopay.config].cs.clientId ${GOPAY_CS_CLIENT_ID}
 yq write --inplace app/config/parameters.yml parameters[gopay.config].cs.clientSecret ${GOPAY_CS_CLIENT_SECRET}
 yq write --inplace app/config/parameters.yml parameters[gopay.config].isProductionMode ${GOPAY_IS_PRODUCTION_MODE}
+
+#GoPay SK
+yq write --inplace app/config/parameters.yml parameters[gopay.config].sk.goid ${GOPAY_SK_GO_ID}
+yq write --inplace app/config/parameters.yml parameters[gopay.config].sk.clientId ${GOPAY_SK_CLIENT_ID}
+yq write --inplace app/config/parameters.yml parameters[gopay.config].sk.clientSecret ${GOPAY_SK_CLIENT_SECRET}
 
 # Replace bucket name for S3 images URL
 sed -i "s/S3_BUCKET_NAME/${S3_API_BUCKET_NAME}/g" docker/nginx/s3/nginx.conf
