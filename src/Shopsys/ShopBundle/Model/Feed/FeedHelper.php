edee61bddca5c7663cffede7bbb141d19573ee1f
Pavel Simek <pavel.simek@shopsys.com>
2019-10-16 08:06:35 +0200

    google and heureka feed: remove adding color and size to product name because color and size are already in product name from DB

--- src/Shopsys/ShopBundle/Model/Feed/FeedHelper.php
+++ src/Shopsys/ShopBundle/Model/Feed/FeedHelper.php
@@ -6,23 +6,9 @@ namespace Shopsys\ShopBundle\Model\Feed;
 
 use Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig;
 use Shopsys\FrameworkBundle\Model\Product\Product;
-use Shopsys\ShopBundle\Model\Product\ProductCachedAttributesFacade;
 
 class FeedHelper
 {
-    /**
-     * @var \Shopsys\ShopBundle\Model\Product\ProductCachedAttributesFacade
-     */
-    private $productCachedAttributesFacade;
-
-    /**
-     * @param \Shopsys\ShopBundle\Model\Product\ProductCachedAttributesFacade $productCachedAttributesFacade
-     */
-    public function __construct(ProductCachedAttributesFacade $productCachedAttributesFacade)
-    {
-        $this->productCachedAttributesFacade = $productCachedAttributesFacade;
-    }
-
     /**
      * @param \Shopsys\FrameworkBundle\Model\Product\Product $product
      * @param \Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig $domainConfig
@@ -37,14 +23,6 @@ class FeedHelper
             return $productName;
         }
 
-        $productDistinguishingParameterValue = $this->productCachedAttributesFacade->getProductDistinguishingParameterValue(
-            $product,
-            $domainConfig->getLocale()
-        );
-
-        $colorValue = $productDistinguishingParameterValue->getFirstDistinguishingParameterValue();
-        $sizeValue = $productDistinguishingParameterValue->getSecondDistinguishingParameterValue();
-
-        return sprintf('%s %s %s %s', $brandName, $productName, $colorValue, $sizeValue);
+        return sprintf('%s %s', $brandName, $productName);
     }
 }
