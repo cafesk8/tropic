88702ce98e88f0d80788e648fc4d1a315cc8ed6e
Pavel Simek <pavel.simek@shopsys.com>
2019-10-22 13:12:23 +0200

    zbozi product feed: add brand name to main variant or variants

--- src/Shopsys/ShopBundle/Model/Feed/Zbozi/FeedItem/ZboziFeedItemFactory.php
+++ src/Shopsys/ShopBundle/Model/Feed/Zbozi/FeedItem/ZboziFeedItemFactory.php
@@ -5,13 +5,41 @@ declare(strict_types=1);
 namespace Shopsys\ShopBundle\Model\Feed\Zbozi\FeedItem;
 
 use Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig;
+use Shopsys\FrameworkBundle\Model\Category\CategoryFacade;
+use Shopsys\FrameworkBundle\Model\Product\Collection\ProductParametersBatchLoader;
+use Shopsys\FrameworkBundle\Model\Product\Collection\ProductUrlsBatchLoader;
+use Shopsys\FrameworkBundle\Model\Product\Pricing\ProductPriceCalculationForUser;
 use Shopsys\FrameworkBundle\Model\Product\Product;
 use Shopsys\ProductFeed\ZboziBundle\Model\FeedItem\ZboziFeedItem;
 use Shopsys\ProductFeed\ZboziBundle\Model\FeedItem\ZboziFeedItemFactory as BaseZboziFeedItemFactory;
 use Shopsys\ProductFeed\ZboziBundle\Model\Product\ZboziProductDomain;
+use Shopsys\ShopBundle\Model\Feed\FeedHelper;
 
 class ZboziFeedItemFactory extends BaseZboziFeedItemFactory
 {
+    /**
+     * @var \Shopsys\ShopBundle\Model\Feed\FeedHelper
+     */
+    private $feedHelper;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\Pricing\ProductPriceCalculationForUser $productPriceCalculationForUser
+     * @param \Shopsys\FrameworkBundle\Model\Product\Collection\ProductUrlsBatchLoader $productUrlsBatchLoader
+     * @param \Shopsys\FrameworkBundle\Model\Product\Collection\ProductParametersBatchLoader $productParametersBatchLoader
+     * @param \Shopsys\FrameworkBundle\Model\Category\CategoryFacade $categoryFacade
+     * @param \Shopsys\ShopBundle\Model\Feed\FeedHelper $feedHelper
+     */
+    public function __construct(
+        ProductPriceCalculationForUser $productPriceCalculationForUser,
+        ProductUrlsBatchLoader $productUrlsBatchLoader,
+        ProductParametersBatchLoader $productParametersBatchLoader,
+        CategoryFacade $categoryFacade,
+        FeedHelper $feedHelper
+    ) {
+        parent::__construct($productPriceCalculationForUser, $productUrlsBatchLoader, $productParametersBatchLoader, $categoryFacade);
+        $this->feedHelper = $feedHelper;
+    }
+
     /**
      * @param \Shopsys\ShopBundle\Model\Product\Product $product
      * @param \Shopsys\ProductFeed\ZboziBundle\Model\Product\ZboziProductDomain|null $zboziProductDomain
@@ -26,10 +54,13 @@ class ZboziFeedItemFactory extends BaseZboziFeedItemFactory
         $cpc = $zboziProductDomain !== null ? $zboziProductDomain->getCpc() : null;
         $cpcSearch = $zboziProductDomain !== null ? $zboziProductDomain->getCpcSearch() : null;
 
+        $brandName = $this->getBrandName($product);
+        $productName = $this->feedHelper->createProductName($product, $domainConfig, $brandName);
+
         return new ZboziFeedItem(
             $product->getId(),
             $mainVariantId,
-            $product->getName($domainConfig->getLocale()),
+            $productName,
             $productForDescription->getDescription($domainConfig->getId()),
             $this->productUrlsBatchLoader->getProductUrl($product, $domainConfig),
             $this->productUrlsBatchLoader->getProductImageUrl($product, $domainConfig),
