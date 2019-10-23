d97a275c8c98e5d07a24d666c144630b47ca1149
Pavel Simek <pavel.simek@shopsys.com>
2019-10-23 09:44:39 +0200

    product stock cron module: download all once a day at 03:00

--- src/Shopsys/ShopBundle/Model/Product/StoreStock/Transfer/StoreStockTransferMapper.php
+++ src/Shopsys/ShopBundle/Model/Product/StoreStock/Transfer/StoreStockTransferMapper.php
@@ -39,21 +39,16 @@ class StoreStockTransferMapper
      * @param \Shopsys\ShopBundle\Model\Product\StoreStock\Transfer\StoreStockTransferResponseItemData $productTransferResponseItemData
      * @param \Shopsys\ShopBundle\Model\Product\Product $product
      * @param \Shopsys\ShopBundle\Component\Transfer\Logger\TransferLogger $transferLogger
-     * @param string $importType
      * @return \Shopsys\ShopBundle\Model\Product\ProductData
      */
     public function mapTransferDataToProductData(
         StoreStockTransferResponseItemData $productTransferResponseItemData,
         Product $product,
-        TransferLogger $transferLogger,
-        string $importType
+        TransferLogger $transferLogger
     ): ProductData {
         $productData = $this->productDataFactory->createFromProduct($product);
 
-        if ($importType === StoreStockImportCronModule::IMPORT_TYPE_IMPORT_ALL) {
-            $productData->stockQuantityByStoreId = [];
-        }
-
+        $productData->stockQuantityByStoreId = [];
         foreach ($productTransferResponseItemData->getSitesQuantity() as $stockQuantity) {
             $store = $this->storeFacade->findByExternalNumber($stockQuantity->getSiteNumber());
 
