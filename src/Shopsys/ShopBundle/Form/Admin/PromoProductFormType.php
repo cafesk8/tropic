69d1f19435933ef539bf595500a1955a174cfd6c
Pavel Simek <pavel.simek@shopsys.com>
2019-11-28 09:17:25 +0100

    admin: currency symbol and constraints

--- src/Shopsys/ShopBundle/Form/Admin/PromoProductFormType.php
+++ src/Shopsys/ShopBundle/Form/Admin/PromoProductFormType.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Shopsys\ShopBundle\Form\Admin;
 
+use Shopsys\FrameworkBundle\Component\Domain\AdminDomainTabsFacade;
+use Shopsys\FrameworkBundle\Form\Constraints\NotNegativeMoneyAmount;
 use Shopsys\FrameworkBundle\Form\ProductType;
 use Shopsys\ShopBundle\Model\Product\PromoProduct\PromoProduct;
 use Shopsys\ShopBundle\Model\Product\PromoProduct\PromoProductData;
+use Shopsys\ShopBundle\Twig\PriceExtension;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -17,6 +20,28 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 class PromoProductFormType extends AbstractType
 {
     /**
+     * @var \Shopsys\FrameworkBundle\Component\Domain\AdminDomainTabsFacade
+     */
+    private $adminDomainTabsFacade;
+
+    /**
+     * @var \Shopsys\ShopBundle\Twig\PriceExtension
+     */
+    private $priceExtension;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\Domain\AdminDomainTabsFacade $adminDomainTabsFacade
+     * @param \Shopsys\ShopBundle\Twig\PriceExtension $priceExtension
+     */
+    public function __construct(
+        AdminDomainTabsFacade $adminDomainTabsFacade,
+        PriceExtension $priceExtension
+    ) {
+        $this->adminDomainTabsFacade = $adminDomainTabsFacade;
+        $this->priceExtension = $priceExtension;
+    }
+
+    /**
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
      * @param array $options
      */
@@ -25,6 +50,8 @@ class PromoProductFormType extends AbstractType
         /* @var \Shopsys\ShopBundle\Model\Product\PromoProduct\PromoProduct|null $promoProduct */
         $promoProduct = $options['promoProduct'];
         $product = $promoProduct === null ? null : $promoProduct->getProduct();
+
+        $currencySymbol = $this->priceExtension->getCurrencyCodeByDomainId($this->adminDomainTabsFacade->getSelectedDomainId());
 
         $builder
             ->add('product', ProductType::class, [
@@ -38,12 +65,22 @@ class PromoProductFormType extends AbstractType
                 ],
             ])
             ->add('price', MoneyType::class, [
+                'scale' => 6,
                 'required' => false,
-                'label' => t('Cena promo produktu'),
+                'label' => t(sprintf('Cena promo produktu (v %s)', $currencySymbol)),
+                'invalid_message' => 'Please enter price in correct format (positive number with decimal separator)',
+                'constraints' => [
+                    new NotNegativeMoneyAmount(['message' => 'Price must be greater or equal to zero']),
+                ],
             ])
             ->add('minimalCartPrice', MoneyType::class, [
+                'scale' => 6,
                 'required' => false,
-                'label' => t('Minimální cena košíku'),
+                'label' => t(sprintf('Minimální cena košíku (v %s)', $currencySymbol)),
+                'invalid_message' => 'Please enter price in correct format (positive number with decimal separator)',
+                'constraints' => [
+                    new NotNegativeMoneyAmount(['message' => 'Price must be greater or equal to zero']),
+                ],
             ])
             ->add('save', SubmitType::class);
     }
