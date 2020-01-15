6d9a9bd0832dd296662a767a57f3f410a20ec93e
Rostislav Vitek <rostislav.vitek@shopsys.com>
2020-01-15 09:11:19 +0100

    CronFacade: remove unused method runScheduledModules()
    - the method was deprecated in framework in https://github.com/shopsys/shopsys/pull/817
    - the method was removed from framework in https://github.com/shopsys/shopsys/pull/1193

--- src/Shopsys/ShopBundle/Component/Cron/CronFacade.php
+++ src/Shopsys/ShopBundle/Component/Cron/CronFacade.php
@@ -9,19 +9,6 @@ class CronFacade extends \Shopsys\FrameworkBundle\Component\Cron\CronFacade
     /**
      * @inheritDoc
      */
-    public function runScheduledModules()
-    {
-        $cronModuleExecutor = new CronModuleExecutor(self::TIMEOUT_SECONDS);
-
-        $cronModuleConfigs = $this->cronConfig->getAllCronModuleConfigs();
-
-        $scheduledCronModuleConfigs = $this->cronModuleFacade->getOnlyScheduledCronModuleConfigs($cronModuleConfigs);
-        $this->runModules($cronModuleExecutor, $scheduledCronModuleConfigs);
-    }
-
-    /**
-     * @inheritDoc
-     */
     public function runScheduledModulesForInstance(string $instanceName): void
     {
         $cronModuleExecutor = new CronModuleExecutor(self::TIMEOUT_SECONDS);
