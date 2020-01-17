4e80fcb00ee9726d454d44d5453fa312796ec667
Lukáš Kielar <lukas.kielar@shopsys.com>
2020-01-17 09:20:32 +0100

    [TK-39640] Changed the text of article "Historie společnosti" to Czech Lorem Ipsum.

--- src/Shopsys/ShopBundle/DataFixtures/Demo/ArticleDataFixture.php
+++ src/Shopsys/ShopBundle/DataFixtures/Demo/ArticleDataFixture.php
@@ -68,7 +68,7 @@ class ArticleDataFixture extends AbstractReferenceFixture
         $this->createArticle($articleData);
 
         $articleData->name = 'Historie společnosti';
-        $articleData->text = 'Počátek firmy se datuje rokem 1990, kdy její hlavní činností bylo zásobování bank a jiných firem papírenským zbožím. Dlouho u toho nezůstalo. Prudký vývoj utvrdil Papírnictví Pavlik CZ v pozici předního velkoobchodu, eskaloval v založení šestnácti vlastních prodejen – a v neposlední řadě umožnil založení vlastního eshopu.';
+        $articleData->text = 'Vítr skoro nefouká a tak by se na první pohled mohlo zdát, že se balónky snad vůbec nepohybují. Jenom tak klidně levitují ve vzduchu. Jelikož slunce jasně září a na obloze byste od východu k západu hledali mráček marně, balónky působí jako jakási fata morgána uprostřed pouště. Zkrátka široko daleko nikde nic, jen zelenkavá tráva, jasně modrá obloha a tři křiklavě barevné pouťové balónky, které se téměř nepozorovatelně pohupují ani ne moc vysoko, ani moc nízko nad zemí. Kdyby pod balónky nebyla sytě zelenkavá tráva, ale třeba suchá silnice či beton, možná by bylo vidět jejich barevné stíny - to jak přes poloprůsvitné barevné balónky prochází ostré sluneční paprsky.';
         $articleData->placement = Article::PLACEMENT_ABOUT;
         $this->createArticle($articleData);
 
