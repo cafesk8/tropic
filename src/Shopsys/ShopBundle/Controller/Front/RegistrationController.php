cc7173c96965d293e7a0efc4cddfba2cdeb9c4c9
Rostislav Vitek <rostislav.vitek@shopsys.com>
2020-01-27 09:22:01 +0100

    redirect logged user from registration page to homepage
    - see https://github.com/shopsys/shopsys/pull/1285

--- src/Shopsys/ShopBundle/Controller/Front/RegistrationController.php
+++ src/Shopsys/ShopBundle/Controller/Front/RegistrationController.php
@@ -9,6 +9,7 @@ use Shopsys\FrameworkBundle\Model\Customer\Mail\CustomerMailFacade;
 use Shopsys\FrameworkBundle\Model\LegalConditions\LegalConditionsFacade;
 use Shopsys\FrameworkBundle\Model\Mail\Exception\MailException;
 use Shopsys\FrameworkBundle\Model\Security\Authenticator;
+use Shopsys\FrameworkBundle\Model\Security\Roles;
 use Shopsys\ShopBundle\Component\CardEan\CardEanFacade;
 use Shopsys\ShopBundle\Component\Setting\Setting;
 use Shopsys\ShopBundle\Form\Front\Registration\RegistrationFormType;
@@ -106,6 +107,10 @@ class RegistrationController extends FrontBaseController
      */
     public function registerAction(Request $request)
     {
+        if ($this->isGranted(Roles::ROLE_LOGGED_CUSTOMER)) {
+            return $this->redirectToRoute('front_homepage');
+        }
+
         $domainId = $this->domain->getId();
         $customerData = $this->customerDataFactory->createForDomainId($domainId);
 
