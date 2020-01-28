d22f3ca0355d9ba7b96db504f37a7e38f05e3776
Rostislav Vitek <rostislav.vitek@shopsys.com>
2020-01-28 14:14:02 +0100

    InfoRowDataFixture now use translator
    - see https://github.com/shopsys/shopsys/pull/1425

--- src/Shopsys/ShopBundle/DataFixtures/Demo/InfoRowDataFixture.php
+++ src/Shopsys/ShopBundle/DataFixtures/Demo/InfoRowDataFixture.php
@@ -6,6 +6,7 @@ namespace Shopsys\ShopBundle\DataFixtures\Demo;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Shopsys\FrameworkBundle\Component\DataFixture\AbstractReferenceFixture;
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
 use Shopsys\ShopBundle\Component\InfoRow\InfoRowFacade;
 
 class InfoRowDataFixture extends AbstractReferenceFixture
@@ -16,12 +17,20 @@ class InfoRowDataFixture extends AbstractReferenceFixture
     private $infoRowFacade;
 
     /**
+     * @var \Shopsys\FrameworkBundle\Component\Domain\Domain
+     */
+    private $domain;
+
+    /**
      * @param \Shopsys\ShopBundle\Component\InfoRow\InfoRowFacade $infoRowFacade
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
      */
     public function __construct(
-        InfoRowFacade $infoRowFacade
+        InfoRowFacade $infoRowFacade,
+        Domain $domain
     ) {
         $this->infoRowFacade = $infoRowFacade;
+        $this->domain = $domain;
     }
 
     /**
@@ -29,10 +38,12 @@ class InfoRowDataFixture extends AbstractReferenceFixture
      */
     public function load(ObjectManager $manager)
     {
-        $this->infoRowFacade->setInfoRow(
-            true,
-            'Upozornenie pre našich zákazníkov, táto verzia e-shopu obsahuje testovacie dáta.',
-            2
-        );
+        foreach ($this->domain->getAll() as $domainConfig) {
+            $this->infoRowFacade->setInfoRow(
+                true,
+                t('Upozornění pro naše zákazníky - tato verze e-shopu obsahuje testovací data.', [], 'dataFixtures', $domainConfig->getLocale()),
+                $domainConfig->getId()
+            );
+        }
     }
 }
