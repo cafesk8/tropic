275712d086c001a99aa922562e033853bb9fed0e
Rostislav Vitek <rostislav.vitek@shopsys.com>
2020-02-05 21:53:15 +0100

    contact form main text now can be managed in admin
    - see https://github.com/shopsys/shopsys/pull/1522

--- src/Controller/Front/ContactFormController.php
+++ src/Controller/Front/ContactFormController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Front;
 
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Model\ContactForm\ContactFormSettingsFacade;
 use Shopsys\FrameworkBundle\Model\LegalConditions\LegalConditionsFacade;
 use App\Form\Front\Contact\ContactFormType;
 use App\Model\ContactForm\ContactFormData;
@@ -30,18 +31,26 @@ class ContactFormController extends FrontBaseController
     private $domain;
 
     /**
+     * @var \Shopsys\FrameworkBundle\Model\ContactForm\ContactFormSettingsFacade
+     */
+    private $contactFormSettingsFacade;
+
+    /**
      * @param \App\Model\ContactForm\ContactFormFacade $contactFormFacade
      * @param \Shopsys\FrameworkBundle\Model\LegalConditions\LegalConditionsFacade $legalConditionsFacade
      * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
+     * @param \Shopsys\FrameworkBundle\Model\ContactForm\ContactFormSettingsFacade $contactFormSettingsFacade
      */
     public function __construct(
         ContactFormFacade $contactFormFacade,
         LegalConditionsFacade $legalConditionsFacade,
-        Domain $domain
+        Domain $domain,
+        ContactFormSettingsFacade $contactFormSettingsFacade
     ) {
         $this->contactFormFacade = $contactFormFacade;
         $this->legalConditionsFacade = $legalConditionsFacade;
         $this->domain = $domain;
+        $this->contactFormSettingsFacade = $contactFormSettingsFacade;
     }
 
     /**
@@ -93,6 +102,7 @@ class ContactFormController extends FrontBaseController
         return $this->render('Front/Content/ContactForm/contactForm.html.twig', [
             'form' => $form->createView(),
             'privacyPolicyArticle' => $privacyPolicyArticle,
+            'mainText' => $this->contactFormSettingsFacade->getMainText($this->domain->getId())
         ]);
     }
 }
