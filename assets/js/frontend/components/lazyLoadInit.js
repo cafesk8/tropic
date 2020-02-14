799fdb2add2b2b47bd63faf5d116b98c5ae1d90b
Rostislav Vitek <rostislav.vitek@shopsys.com>
2020-02-14 15:22:28 +0100

    remove instagram related files
    - the files were added by mistake during upgrade to flex structure

--- assets/js/frontend/components/lazyLoadInit.js
+++ assets/js/frontend/components/lazyLoadInit.js
@@ -14,26 +14,4 @@ export function lazyLoadCall (container) {
     });
 }
 
-export function manualReplaceBackgroundImage ($lazyloadedImage) {
-    $lazyloadedImage.css('background', 'url(' + $lazyloadedImage.attr('data-original') + ')');
-    $lazyloadedImage.removeClass('js-lazy-load-on-background');
-};
-
-$(window).scroll(function (event) {
-    const $instagramScrollPoint = $('.js-instagram-scroll-point');
-    if ($instagramScrollPoint.length > 0) {
-        const positionTop = $instagramScrollPoint.offset().top;
-        const elementHeight = $instagramScrollPoint.outerHeight();
-        const windowHeight = $(window).height();
-        const windowScroll = $(event.currentTarget).scrollTop() + 1000;
-
-        if (windowScroll > (positionTop + elementHeight - windowHeight)) {
-            $('body').filterAllNodes('.js-lazy-load-on-background').each(function () {
-                const $lazyloadedImage = $(this);
-                manualReplaceBackgroundImage($lazyloadedImage);
-            });
-        }
-    }
-});
-
 (new Register()).registerCallback(lazyLoadCall);
