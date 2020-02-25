c5bd36a9c28a150885ec99b2ab41f5dfd380bd17
Lukáš Kielar <lukas.kielar@shopsys.com>
2020-02-25 15:06:55 +0100

    Added an option to change payment method when customer returns from GoPay without successfully paying for the order.

---
+++ assets/js/frontend/order/changeOrderPayment.js
@@ -0,0 +1,23 @@
+import Register from 'framework/common/utils/register';
+
+export default class ChangeOrderPayment {
+    onOrderPaymentChoose (event) {
+        var $form = event.currentTarget.closest('form');
+        $form.submit();
+    }
+
+    onOrderGoPayPaymentChoose (event) {
+        $('.js-gopay-bank-swifts').toggleClass('display-none', !$(event.currentTarget).is(':checked'));
+    }
+
+    static init ($container) {
+        var $orderPayments = $container.filterAllNodes('.js-order-change-payment');
+        var $orderGoPayPayments = $container.filterAllNodes('.js-gopay-bank-transfer-input');
+        const changeOrderPayment = new ChangeOrderPayment();
+
+        $orderPayments.change(changeOrderPayment.onOrderPaymentChoose);
+        $orderGoPayPayments.click(changeOrderPayment.onOrderGoPayPaymentChoose);
+    }
+}
+
+(new Register()).registerCallback(ChangeOrderPayment.init);
