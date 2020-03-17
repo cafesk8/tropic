dd096aca0256d32fc49736710870b72fd7da2b65
Rostislav Vitek <rostislav.vitek@shopsys.com>
2020-03-17 15:03:17 +0100

    [BG-6907] FE product list: proper displaying of products
    - do not display products with (calculatedSellingDenied === true) in lists
    - display link to product detail for main variant
    - disable product action form for out of stock products

--- src/Model/Product/ProductOnCurrentDomainElasticFacade.php
+++ src/Model/Product/ProductOnCurrentDomainElasticFacade.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace App\Model\Product;
 
-use Shopsys\FrameworkBundle\Component\Paginator\PaginationResult;
-use Shopsys\FrameworkBundle\Model\Product\Filter\ProductFilterData;
-use Shopsys\FrameworkBundle\Model\Product\Listing\ProductListOrderingConfig;
 use Shopsys\FrameworkBundle\Model\Product\ProductOnCurrentDomainElasticFacade as BaseProductOnCurrentDomainElasticFacade;
-use Shopsys\FrameworkBundle\Model\Product\Search\FilterQuery;
 
 /**
  * @property \App\Model\Product\Search\FilterQueryFactory $filterQueryFactory
@@ -20,66 +16,6 @@ use Shopsys\FrameworkBundle\Model\Product\Search\FilterQuery;
  */
 class ProductOnCurrentDomainElasticFacade extends BaseProductOnCurrentDomainElasticFacade
 {
-    /**
-     * @param \Shopsys\FrameworkBundle\Model\Product\Filter\ProductFilterData $productFilterData
-     * @param string $orderingModeId
-     * @param int $page
-     * @param int $limit
-     * @return \App\Model\Product\Search\FilterQuery
-     */
-    protected function createFilterQueryWithProductFilterData(ProductFilterData $productFilterData, string $orderingModeId, int $page, int $limit): FilterQuery
-    {
-        $filterQuery = $this->filterQueryFactory->create($this->getIndexName())
-            ->filterOnlyVisible($this->currentCustomerUser->getPricingGroup())
-            ->setPage($page)
-            ->setLimit($limit)
-            ->applyOrdering($orderingModeId, $this->currentCustomerUser->getPricingGroup());
-
-        $filterQuery = $this->productFilterDataToQueryTransformer->addBrandsToQuery($productFilterData, $filterQuery);
-        $filterQuery = $this->productFilterDataToQueryTransformer->addFlagsToQuery($productFilterData, $filterQuery);
-        $filterQuery = $this->productFilterDataToQueryTransformer->addParametersToQuery($productFilterData, $filterQuery);
-        $filterQuery = $this->productFilterDataToQueryTransformer->addStockToQuery($productFilterData, $filterQuery);
-        $filterQuery = $this->productFilterDataToQueryTransformer->addPricesToQuery($productFilterData, $filterQuery, $this->currentCustomerUser->getPricingGroup());
-        /** @var \App\Model\Product\Search\FilterQuery $filterQuery */
-        return $filterQuery;
-    }
-
-    /**
-     * @param string $orderingModeId
-     * @param int $page
-     * @param int $limit
-     * @param int $brandId
-     * @return \Shopsys\FrameworkBundle\Component\Paginator\PaginationResult
-     */
-    public function getPaginatedProductsForBrand(string $orderingModeId, int $page, int $limit, int $brandId): PaginationResult
-    {
-        $emptyProductFilterData = new ProductFilterData();
-
-        $filterQuery = $this->createListableProductsForBrandFilterQuery($emptyProductFilterData, $orderingModeId, $page, $limit, $brandId);
-
-        $productsResult = $this->productElasticsearchRepository->getSortedProductsResultByFilterQuery($filterQuery);
-
-        return new PaginationResult($page, $limit, $productsResult->getTotal(), $productsResult->getHits());
-    }
-
-    /**
-     * {@inheritdoc}
-     * Copy pasted from parent to create the proper instance of ProductFilterData - the class is overridden in this project
-     */
-    public function getSearchAutocompleteProducts(?string $searchText, int $limit): PaginationResult
-    {
-        $emptyProductFilterData = new ProductFilterData();
-        $page = 1;
-
-        $filterQuery = $this->createListableProductsForSearchTextFilterQuery($emptyProductFilterData, ProductListOrderingConfig::ORDER_BY_RELEVANCE, $page, $limit, $searchText);
-
-        $productIds = $this->productElasticsearchRepository->getSortedProductIdsByFilterQuery($filterQuery);
-
-        $visibleExcludingVariantsByIds = $this->productRepository->getVisibleExcludingVariantsByIds($this->domain->getId(), $this->currentCustomerUser->getPricingGroup(), $productIds->getIds());
-
-        return new PaginationResult($page, $limit, $productIds->getTotal(), $visibleExcludingVariantsByIds);
-    }
-
     /**
      * @param int[] $ids
      * @return array
