aa5bc9617b11645b693a8d562e4bbd325a6f57bd
Lukáš Kielar <lukas.kielar@shopsys.com>
2020-04-14 18:59:58 +0200

    Sale price is now set to null by default during import from Pohoda so that the previous price is removed when the product is removed from sale stocks.

--- src/Model/Product/Transfer/PohodaProductMapper.php
+++ src/Model/Product/Transfer/PohodaProductMapper.php
@@ -62,6 +62,7 @@ class PohodaProductMapper
         $productData->registrationDiscountDisabled = $pohodaProduct->registrationDiscountDisabled;
 
         foreach ($this->domain->getAllIds() as $domainId) {
+            $salePricingGroupId = $this->pricingGroupFacade->getSalePricePricingGroup($domainId)->getId();
             $productData->manualInputPricesByPricingGroupId[$this->pricingGroupFacade->getByNameAndDomainId(
                 PricingGroup::PRICING_GROUP_ORDINARY_CUSTOMER,
                 $domainId
@@ -74,11 +75,12 @@ class PohodaProductMapper
                 $this->pricingGroupFacade->getStandardPricePricingGroup($domainId)->getId()
             ] = $this->getPriceFromString($pohodaProduct->standardPrice);
 
+            $productData->manualInputPricesByPricingGroupId[$salePricingGroupId] = null;
+
             foreach (PohodaProductExportRepository::SALE_STOCK_IDS_ORDERED_BY_PRIORITY as $stockId) {
                 if (isset($pohodaProduct->saleInformation[$stockId])) {
-                    $productData->manualInputPricesByPricingGroupId[
-                    $this->pricingGroupFacade->getSalePricePricingGroup($domainId)->getId()
-                    ] = Money::create($this->fixInvalidPriceFormat($pohodaProduct->saleInformation[$stockId]));
+                    $productData->manualInputPricesByPricingGroupId[$salePricingGroupId] =
+                        $this->getPriceFromString($pohodaProduct->saleInformation[$stockId]);
                     break;
                 }
             }
