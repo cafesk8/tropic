52451075eeb3d1b74b7b5f8de1983e77019765a9
Jan Roula <roula@bartonstudio.cz>
2020-04-17 12:43:08 +0200

    [TF-81] Togglable choices in filter - always show checked filter values after reload

--- assets/js/frontend/product/productFilterBox.js
+++ assets/js/frontend/product/productFilterBox.js
@@ -4,17 +4,33 @@ export default class ProductFilterBox {
 
     constructor ($container) {
         const _this = this;
-        $container.filterAllNodes('.js-product-filter-box-button').on('click', event => {
-            _this.toggleFilterBox($(event.target).closest('.js-product-filter-box'));
+        $container.filterAllNodes('.js-product-filter-box-button-show').on('click', event => {
+            _this.showFilterBox($(event.target).closest('.js-product-filter-box'));
+        });
+        $container.filterAllNodes('.js-product-filter-box-button-hide').on('click', event => {
+            _this.hideFilterBox($(event.target).closest('.js-product-filter-box'));
         });
     }
 
-    toggleFilterBox ($parameterContainer) {
-        const $productFilterLabel = $parameterContainer.find('.js-product-filter-box-label');
-        $productFilterLabel.toggleClass('inactive');
+    showFilterBox ($parameterContainer) {
+        $parameterContainer.find('.js-product-filter-box-label').each(function () {
+            $(this).removeClass('inactive');
+        });
 
-        const $productFilterButton = $parameterContainer.find('.js-product-filter-box-button');
-        $productFilterButton.toggleClass('inactive');
+        $parameterContainer.find('.js-product-filter-box-button-show').toggleClass('inactive');
+        $parameterContainer.find('.js-product-filter-box-button-hide').toggleClass('inactive');
+    }
+
+    hideFilterBox ($parameterContainer) {
+        $parameterContainer.find('.js-product-filter-box-label').each(function () {
+            const $choiceInput = $(this).find('input');
+            if ($choiceInput.prop('checked') === false) {
+                $(this).addClass('inactive');
+            }
+        });
+
+        $parameterContainer.find('.js-product-filter-box-button-show').toggleClass('inactive');
+        $parameterContainer.find('.js-product-filter-box-button-hide').toggleClass('inactive');
     }
 
     static init ($container) {
