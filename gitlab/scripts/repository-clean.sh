04ea83614576793c26cd11d3c965528afe0f73e7
Jan Górecki <jan.gorecki@shopsys.com>
2020-04-28 11:37:06 +0200

    prevent to delete deployed tag for production and devel

--- gitlab/scripts/repository-clean.sh
+++ gitlab/scripts/repository-clean.sh
@@ -23,6 +23,9 @@ echo "cleaning container registry for deleted branches: "
 PROJECT_BRANCHES="$(curl -L --silent --header "PRIVATE-TOKEN: ${API_TOKEN}" "${API_URL}/repository/branches" | jq -r '.[].name' )"
 REGISTRY_REPOSITORIES="$(curl -L --silent --header "PRIVATE-TOKEN: ${API_TOKEN}" "${API_URL}/registry/repositories")"
 
+DEPLOYED_TAG_FOR_PRODUCTION=$(curl -L --silent --header "PRIVATE-TOKEN: ${API_TOKEN}" "${API_URL}/deployments?environment=production&status=success&sort=desc" | jq -r '.[0].sha')
+DEPLOYED_TAG_FOR_DEVEL=$(curl -L --silent --header "PRIVATE-TOKEN: ${API_TOKEN}" "${API_URL}/deployments?environment=devel&status=success&sort=desc" | jq -r '.[0].sha')
+
 for REGISTRY_REPOSITORY in $(echo "${REGISTRY_REPOSITORIES}" | jq -rc '.[]'); do
   REPOSITORY_ID=$(echo "${REGISTRY_REPOSITORY}" | jq -r '.id')
   REPOSITORY_NAME=$(echo "${REGISTRY_REPOSITORY}" | jq -r '.name')
@@ -34,7 +37,8 @@ for REGISTRY_REPOSITORY in $(echo "${REGISTRY_REPOSITORIES}" | jq -rc '.[]'); do
       CURL_OUTPUT=$(curl -L --silent --header "PRIVATE-TOKEN: ${API_TOKEN}" \
       -X DELETE \
       --data 'keep_n=3' \
-      --data "name_regex=*" \
+      --data "name_regex_delete=*" \
+      --data "name_regex_keep=${DEPLOYED_TAG_FOR_PRODUCTION}|${DEPLOYED_TAG_FOR_DEVEL}" \
       "${API_URL}/registry/repositories/${REPOSITORY_ID}/tags")
           echo -e "    ${REPOSITORY_NAME} --> ${YELLOW}DELETED OLD TAGS!${NO_COLOR} (${CURL_OUTPUT})"
   fi
