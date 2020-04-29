8fbf7be73e4c487cc2059451d666490063e6d336
Stankovic <lukas.stankovic@shopsys.com>
2020-04-29 15:56:26 +0200

    [TF-14] Fix editing product with Product Groups (action sets) in admin

--- src/Form/ProductGroupItemsListType.php
+++ src/Form/ProductGroupItemsListType.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace App\Form;
 
-use App\Form\Transformers\ProductGroupItemsTypeTransformer;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,34 +12,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class ProductGroupItemsListType extends AbstractType
 {
     /**
-     * @var \App\Form\Transformers\ProductGroupItemsTypeTransformer
-     */
-    private $productGroupItemsTypeTransformer;
-
-    /**
-     * @param \App\Form\Transformers\ProductGroupItemsTypeTransformer $productGroupItemsTypeTransformer
-     */
-    public function __construct(ProductGroupItemsTypeTransformer $productGroupItemsTypeTransformer)
-    {
-        $this->productGroupItemsTypeTransformer = $productGroupItemsTypeTransformer;
-    }
-
-    /**
      * {@inheritdoc}
      */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        $view->vars['products'] = $form->getData();
+        $view->vars['product_group_items'] = $form->getData();
         $view->vars['main_product'] = $options['main_product'];
         $view->vars['top_info_title'] = $options['top_info_title'];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->addModelTransformer($this->productGroupItemsTypeTransformer);
     }
 
     /**
