8d2701f10d7afc4d7bacf59947f40925edb644ff
Stankovic <lukas.stankovic@shopsys.com>
2020-05-20 10:38:01 +0200

    [TF-225] Add transfer logger into component

--- src/Component/Transfer/Pohoda/Category/PohodaCategoryExportFacade.php
+++ src/Component/Transfer/Pohoda/Category/PohodaCategoryExportFacade.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace App\Component\Transfer\Pohoda\Category;
 
+use App\Component\Transfer\Logger\TransferLoggerFactory;
 use App\Component\Transfer\Pohoda\Exception\PohodaInvalidDataException;
+use App\Model\Category\Transfer\CategoryImportCronModule;
 use DateTime;
-use Symfony\Bridge\Monolog\Logger;
 
 class PohodaCategoryExportFacade
 {
     /**
-     * @var \Symfony\Bridge\Monolog\Logger
+     * @var \App\Component\Transfer\Logger\TransferLogger
      */
     private $logger;
 
@@ -26,16 +27,16 @@ class PohodaCategoryExportFacade
     private $pohodaCategoryExportRepository;
 
     /**
-     * @param \Symfony\Bridge\Monolog\Logger $logger
+     * @param \App\Component\Transfer\Logger\TransferLoggerFactory $transferLoggerFactory
      * @param \App\Component\Transfer\Pohoda\Category\PohodaCategoryExportRepository $pohodaCategoryExportRepository
      * @param \App\Component\Transfer\Pohoda\Category\PohodaCategoryDataValidator $pohodaCategoryDataValidator
      */
     public function __construct(
-        Logger $logger,
+        TransferLoggerFactory $transferLoggerFactory,
         PohodaCategoryExportRepository $pohodaCategoryExportRepository,
         PohodaCategoryDataValidator $pohodaCategoryDataValidator
     ) {
-        $this->logger = $logger;
+        $this->logger = $transferLoggerFactory->getTransferLoggerByIdentifier(CategoryImportCronModule::TRANSFER_IDENTIFIER);
         $this->pohodaCategoryExportRepository = $pohodaCategoryExportRepository;
         $this->pohodaCategoryDataValidator = $pohodaCategoryDataValidator;
     }
@@ -79,7 +80,7 @@ class PohodaCategoryExportFacade
             try {
                 $this->pohodaCategoryDataValidator->validate($pohodaCategoryData);
             } catch (PohodaInvalidDataException $exc) {
-                $this->logger->addError('Položka není validní a nebude přenesena.', [
+                $this->logger->addError('Položka není validní a nebude exportována z Pohody.', [
                     'pohodaId' => $pohodaCategoryData[PohodaCategory::COL_POHODA_ID],
                     'exceptionMessage' => $exc->getMessage(),
                 ]);
@@ -88,6 +89,7 @@ class PohodaCategoryExportFacade
 
             $pohodaCategories[] = new PohodaCategory($pohodaCategoryData);
         }
+        $this->logger->persistTransferIssues();
 
         return $pohodaCategories;
     }
