cdedc6e42eb253e2e2a7aa58ff3b15c9629565fd
Rostislav Vitek <rostislav.vitek@shopsys.com>
2020-05-23 11:08:09 +0200

    Image: remove $migrateFileName property and all the related logic
    - the property was used for image migration on BSM and is useless for us

--- src/Component/Image/ImageFacade.php
+++ src/Component/Image/ImageFacade.php
@@ -12,7 +12,7 @@ use Shopsys\FrameworkBundle\Component\Image\ImageFacade as BaseImageFacade;
  * @property \Shopsys\FrameworkBundle\Component\EntityExtension\EntityManagerDecorator $em
  * @property \App\Component\FileUpload\FileUpload $fileUpload
  * @property \App\Component\Image\ImageRepository $imageRepository
- * @method __construct(mixed $imageUrlPrefix, \Doctrine\ORM\EntityManagerInterface $em, \Shopsys\FrameworkBundle\Component\Image\Config\ImageConfig $imageConfig, \App\Component\Image\ImageRepository $imageRepository, \League\Flysystem\FilesystemInterface $filesystem, \App\Component\FileUpload\FileUpload $fileUpload, \Shopsys\FrameworkBundle\Component\Image\ImageLocator $imageLocator, \App\Component\Image\ImageFactory $imageFactory, \League\Flysystem\MountManager $mountManager)
+ * @method __construct(mixed $imageUrlPrefix, \Doctrine\ORM\EntityManagerInterface $em, \Shopsys\FrameworkBundle\Component\Image\Config\ImageConfig $imageConfig, \App\Component\Image\ImageRepository $imageRepository, \League\Flysystem\FilesystemInterface $filesystem, \App\Component\FileUpload\FileUpload $fileUpload, \Shopsys\FrameworkBundle\Component\Image\ImageLocator $imageLocator, \Shopsys\FrameworkBundle\Component\Image\ImageFactoryInterface $imageFactory, \League\Flysystem\MountManager $mountManager)
  * @method saveImageOrdering(\App\Component\Image\Image[] $orderedImages)
  * @method deleteImages(object $entity, \App\Component\Image\Image[] $images)
  * @method \App\Component\Image\Image getImageByEntity(object $entity, string|null $type)
@@ -30,49 +30,6 @@ use Shopsys\FrameworkBundle\Component\Image\ImageFacade as BaseImageFacade;
  */
 class ImageFacade extends BaseImageFacade
 {
-    /**
-     * @var \App\Component\Image\ImageFactory
-     */
-    protected $imageFactory;
-
-    /**
-     * @param object $entity
-     * @param string $migrateFilename
-     * @param string|null $type
-     */
-    public function migrateImage(object $entity, string $migrateFilename, ?string $type): void
-    {
-        $imageEntityConfig = $this->imageConfig->getImageEntityConfig($entity);
-        $entityId = $this->getEntityId($entity);
-
-        $newImage = $this->imageFactory->createImageForMigrate(
-            $imageEntityConfig,
-            $entityId,
-            $migrateFilename,
-            $type
-        );
-        $this->em->persist($newImage);
-        $this->em->flush($newImage);
-    }
-
-    /**
-     * @param object $entity
-     */
-    public function deleteImagesFromMigration(object $entity): void
-    {
-        $allImages = $this->getAllImagesByEntity($entity);
-
-        $migratedImages = [];
-        /** @var \App\Component\Image\Image $image */
-        foreach ($allImages as $image) {
-            if ($image->getMigrateFileName() !== null) {
-                $migratedImages[] = $image;
-            }
-        }
-
-        $this->deleteImages($entity, $migratedImages);
-    }
-
     /**
      * @param object $entity
      * @param \Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig $domainConfig
