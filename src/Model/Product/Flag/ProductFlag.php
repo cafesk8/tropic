b1f9412613698651dc5b603f2d455a0cfff4bd89
Lukáš Kielar <lukas.kielar@shopsys.com>
2020-05-28 09:58:32 +0200

    Fixed activity limits for product flags to include whole last day.

--- src/Model/Product/Flag/ProductFlag.php
+++ src/Model/Product/Flag/ProductFlag.php
@@ -106,6 +106,8 @@ class ProductFlag
      */
     public function isActive(): bool
     {
-        return ($this->activeFrom === null || $this->activeFrom->getTimestamp() <= time()) && ($this->activeTo === null || $this->activeTo->getTimestamp() > strtotime('tomorrow'));
+        $activeToTimestamp = $this->activeTo !== null ? $this->activeTo->getTimestamp() : strtotime('tomorrow');
+
+        return ($this->activeFrom === null || $this->activeFrom->getTimestamp() <= time()) && ($this->activeTo === null || $activeToTimestamp + 86400 > time());
     }
 }
