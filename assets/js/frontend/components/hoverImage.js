e3d56ca54060d734955f343d7d06db569d2a6ab1
MichalSrnicek <michal.srnicek@shopsys.com>
2020-07-24 14:08:38 +0200

    design: edit format and add conditions

--- assets/js/frontend/components/hoverImage.js
+++ assets/js/frontend/components/hoverImage.js
@@ -4,15 +4,20 @@ export default class HoverImage {
     static init ($container) {
         var $image = $container.filterAllNodes('.js-hover-image');
 
-        $image.mouseenter(function(){
+        $image.mouseenter(function () {
             var $bigImageSrc = $(this).data('original-image');
+            var $imageExist = $(this).find('.js-hover-image-original').length > 0;
 
-            $(this).append('<div class="list-variants__item__description__image-original js-hover-image-original"><img src="' + $bigImageSrc + '"></div>');
-        })
+            if ($imageExist) {
+                $(this).find('.js-hover-image-original').removeClass('display-none');
+            } else {
+                $(this).append('<div class="list-variants__item__description__image-original js-hover-image-original"><img src="' + $bigImageSrc + '"></div>');
+            };
+        });
 
-        $image.mouseleave(function(){
-            $(this).find('.js-hover-image-original').remove();
-        })
+        $image.mouseleave(function () {
+            $(this).find('.js-hover-image-original').addClass('display-none');
+        });
     }
 }
 
