ec2186a75a409544051611689bfda64d0a0715fd
Rostislav Vitek <rostislav.vitek@shopsys.com>
2020-08-10 18:38:13 +0200

    product images transfer: image description validation

--- src/Component/Transfer/Pohoda/Product/Image/PohodaImageDataValidator.php
+++ src/Component/Transfer/Pohoda/Product/Image/PohodaImageDataValidator.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace App\Component\Transfer\Pohoda\Product\Image;
 
 use App\Component\Transfer\Pohoda\Exception\PohodaInvalidDataException;
+use App\Model\Product\Product;
+use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\Constraints\Collection;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PohodaImageDataValidator
@@ -47,11 +50,41 @@ class PohodaImageDataValidator
                 PohodaImage::ALIAS_PRODUCT_POHODA_ID => [
                     new NotBlank(),
                 ],
+                PohodaImage::ALIAS_DESCRIPTION => [
+                    new Callback(['callback' => [$this, 'validateDescription']]),
+                ],
             ],
         ]));
 
         if (count($violations) > 0) {
             throw new PohodaInvalidDataException($violations);
+        }
+    }
+
+    /**
+     * @param string|null $imageDescription
+     * @param \Symfony\Component\Validator\Context\ExecutionContextInterface $context
+     */
+    public function validateDescription(?string $imageDescription, ExecutionContextInterface $context): void
+    {
+        $invalidMessage = 'Popis obrázku pro set od dodavatele má nevalidní formát (očekává se nenulový počet znaků před i za hvězdičkou, přičemž část za hvězdičkou by měla obsahovat jen číslice)';
+        if ($imageDescription !== null) {
+            if ($imageDescription === '') {
+                $context->addViolation($invalidMessage);
+                return;
+            }
+            $separatorPosition = strpos($imageDescription, Product::SUPPLIER_SET_ITEM_NAME_COUNT_SEPARATOR);
+            if ($separatorPosition !== false) {
+                $supplierSetItemCount = substr($imageDescription, $separatorPosition + 1);
+                $supplierSetItemName = substr($imageDescription, 0, $separatorPosition);
+                if ((int)$supplierSetItemCount === 0
+                    || strlen($supplierSetItemName) === 0
+                    || !preg_match('#^\d+$#', $supplierSetItemCount)
+                ) {
+                    $context->addViolation($invalidMessage);
+                    return;
+                }
+            }
         }
     }
 }
