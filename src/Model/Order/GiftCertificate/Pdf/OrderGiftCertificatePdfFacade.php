8d4cf7f3db33d57f410212c8196d2f5b0fbf6cb7
Lukáš Kielar <lukas.kielar@shopsys.com>
2020-09-07 11:45:06 +0200

    Two PDFs with certificates are now generated. One in colors and second in greyscale.

--- src/Model/Order/GiftCertificate/Pdf/OrderGiftCertificatePdfFacade.php
+++ src/Model/Order/GiftCertificate/Pdf/OrderGiftCertificatePdfFacade.php
@@ -7,50 +7,85 @@ namespace App\Model\Order\GiftCertificate\Pdf;
 use App\Component\FileUpload\FileUpload;
 use App\Model\Order\GiftCertificate\OrderGiftCertificate;
 use Dompdf\Dompdf;
+use League\Flysystem\FileNotFoundException;
+use League\Flysystem\Filesystem;
+use League\Flysystem\FilesystemInterface;
 use Shopsys\FrameworkBundle\Component\UploadedFile\UploadedFileDataFactory;
 use Shopsys\FrameworkBundle\Component\UploadedFile\UploadedFileFacade;
 use Twig\Environment;
 
 class OrderGiftCertificatePdfFacade
 {
-    /**
-     * @var \Twig\Environment
-     */
-    private $twigEnvironment;
+    private Environment $twigEnvironment;
 
-    /**
-     * @var \Shopsys\FrameworkBundle\Component\UploadedFile\UploadedFileFacade
-     */
-    private $uploadedFileFacade;
+    private UploadedFileFacade $uploadedFileFacade;
 
-    /**
-     * @var \App\Component\FileUpload\FileUpload
-     */
-    private $fileUpload;
+    private FileUpload $fileUpload;
 
-    /**
-     * @var \Shopsys\FrameworkBundle\Component\UploadedFile\UploadedFileDataFactory
-     */
-    private $uploadedFileDataFactory;
+    private UploadedFileDataFactory $uploadedFileDataFactory;
+
+    private Filesystem $filesystem;
 
     /**
      * @param \Twig\Environment $twigEnvironment
      * @param \Shopsys\FrameworkBundle\Component\UploadedFile\UploadedFileFacade $uploadedFileFacade
      * @param \App\Component\FileUpload\FileUpload $fileUpload
      * @param \Shopsys\FrameworkBundle\Component\UploadedFile\UploadedFileDataFactory $uploadedFileDataFactory
+     * @param \League\Flysystem\Filesystem $filesystem
      */
-    public function __construct(Environment $twigEnvironment, UploadedFileFacade $uploadedFileFacade, FileUpload $fileUpload, UploadedFileDataFactory $uploadedFileDataFactory)
-    {
+    public function __construct(
+        Environment $twigEnvironment,
+        UploadedFileFacade $uploadedFileFacade,
+        FileUpload $fileUpload,
+        UploadedFileDataFactory $uploadedFileDataFactory,
+        FilesystemInterface $filesystem
+    ) {
         $this->twigEnvironment = $twigEnvironment;
         $this->uploadedFileFacade = $uploadedFileFacade;
         $this->fileUpload = $fileUpload;
         $this->uploadedFileDataFactory = $uploadedFileDataFactory;
+        $this->filesystem = $filesystem;
     }
 
     /**
      * @param \App\Model\Order\GiftCertificate\OrderGiftCertificate $orderGiftCertificate
      */
     public function create(OrderGiftCertificate $orderGiftCertificate): void
+    {
+        $dirName = $this->fileUpload->getTemporaryDirectory();
+
+        if (!is_dir($dirName)) {
+            mkdir($dirName, 0777, true);
+        }
+
+        $uploadedFileData = $this->uploadedFileDataFactory->createByEntity($orderGiftCertificate);
+        $randomHash = substr(md5((string)random_int(0, 1000000)), 0, 4);
+
+        $fileName = t('Darkovy_poukaz_-_barevny') . '_' . $randomHash . '.pdf';
+        $this->filesystem->put($dirName . '/' . $fileName, $this->renderPdf($orderGiftCertificate, false)->output());
+        $fileNames = [$fileName];
+
+        $fileName = t('Darkovy_poukaz_-_cernobily') . '_' . $randomHash . '.pdf';
+        $this->filesystem->put($dirName . '/' . $fileName, $this->renderPdf($orderGiftCertificate, true)->output());
+        $fileNames[] = $fileName;
+        $uploadedFileData->uploadedFiles = $fileNames;
+        $uploadedFileData->uploadedFilenames = $fileNames;
+
+        $this->uploadedFileFacade->manageFiles($orderGiftCertificate, $uploadedFileData);
+
+        try {
+            foreach ($fileNames as $fileName) {
+                $this->filesystem->delete($dirName . '/' . $fileName);
+            }
+        } catch (FileNotFoundException $fileNotFoundException) {}
+    }
+
+    /**
+     * @param \App\Model\Order\GiftCertificate\OrderGiftCertificate $orderGiftCertificate
+     * @param bool $greyscale
+     * @return \Dompdf\Dompdf
+     */
+    private function renderPdf(OrderGiftCertificate $orderGiftCertificate, bool $greyscale): Dompdf
     {
         $dompdf = new Dompdf();
         $giftCertificate = $orderGiftCertificate->getGiftCertificate();
@@ -59,22 +94,12 @@ class OrderGiftCertificatePdfFacade
             'giftCertificateCurrency' => $orderGiftCertificate->getOrder()->getCurrency(),
             'giftCertificateValue' => $giftCertificate->getCertificateValue(),
             'giftCertificateValidTo' => $giftCertificate->isActive() ? $giftCertificate->getValidTo() : null,
+            'greyscale' => $greyscale,
         ]);
         $dompdf->loadHtml($html);
         $dompdf->render();
 
-        $dirName = $this->fileUpload->getTemporaryDirectory();
-
-        if (!is_dir($dirName)) {
-            mkdir($dirName, 0777, true);
-        }
-
-        $fileName = 'GiftCertificate' . strval(time()) . substr(hash('sha256', $orderGiftCertificate->getGiftCertificate()->getCode()), 0, 8) . '.pdf';
-        file_put_contents($dirName . '/' . $fileName, $dompdf->output());
-        $uploadedFileData = $this->uploadedFileDataFactory->createByEntity($orderGiftCertificate);
-        $uploadedFileData->uploadedFiles[] = $fileName;
-        $uploadedFileData->uploadedFilenames[] = $fileName;
-        $this->uploadedFileFacade->manageFiles($orderGiftCertificate, $uploadedFileData);
+        return $dompdf;
     }
 
     /**
