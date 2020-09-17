2783d25e6b91c30edf74a28689801f62ab8c9c68
Rostislav Vitek <rostislav.vitek@shopsys.com>
2020-09-17 14:48:20 +0200

    ProductImportFacade:processImport() now return ids of actually updated products

--- src/Model/Product/Transfer/ProductImportFacade.php
+++ src/Model/Product/Transfer/ProductImportFacade.php
@@ -133,7 +133,7 @@ class ProductImportFacade
             $this->logger->persistTransferIssues();
         }
 
-        return $changedPohodaProductIds;
+        return $this->updatedPohodaProductIds;
     }
 
     /**
