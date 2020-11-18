02ccf6e7eed266e8e864ce3fb8e7f3670f4acebb
Rostislav Vitek <rostislav.vitek@shopsys.com>
2020-11-18 18:43:41 +0100

    Revert "fix boosting products is search"
    This reverts commit 3d9f060f28309ef1bf51ee12ac498afcde28347b.

--- src/Model/Product/Search/FilterQuery.php
+++ src/Model/Product/Search/FilterQuery.php
@@ -57,7 +57,6 @@ class FilterQuery extends BaseFilterQuery
                     'variants_aliases^45',
                     'variants_aliases.edge_ngram_with_diacritic^40',
                     'variants_aliases.edge_ngram_without_diacritic^35',
-                    'boosting_name^1000000',
                 ],
             ],
         ];
