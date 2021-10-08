0b06f8e66f9430a9d88c097ebb3bccf48eb6e3fe
Jan Cafourek <jan.cafourek@gmail.com>
2021-10-08 16:18:14 +0200

    get availability from text

--- assets/js/frontend/product/preorderModal.js
+++ assets/js/frontend/product/preorderModal.js
@@ -10,14 +10,14 @@ export default class PreorderModal {
         }
 
         const productName = $(this).data('product-name');
-		const productType = $(this).data('product-type');
-		let productAvailability;
+        const productType = $(this).data('product-type');
+        let productAvailability;
 
-		if(productType == 'simple') {
-        	productAvailability = $(this).closest('form').find('.in-availability__date').text();
-		} else {
-        	productAvailability = $(this).closest('.list-variants__item').find('.in-availability__date').text();
-		}
+        if (productType === 'simple') {
+            productAvailability = $(document).find('.in-tab__content__item__text ').text();
+        } else {
+            productAvailability = $(document).find('.box-detail__left__description').text();
+        }
 
         const $window = new Window({
             content: '<div class="js-window-content window-popup__in" style="overflow:hidden;">'
@@ -27,7 +27,7 @@ export default class PreorderModal {
                     + '<div class="form-line__input">'
                         + '<input type="text" id="preorder-name" class="input" required name="preorder-name" style="padding-bottom: 15px;" placeholder="Váše jméno">'
                     + '</div>'
-					+ '<div class="form-line__input">'
+                    + '<div class="form-line__input">'
                         + '<input type="email" id="preorder-email" class="input" required name="preorder-email" style="padding-bottom: 15px;" placeholder="Váš e-mail">'
                     + '</div>'
                     + '<div class="form-line__input">'
@@ -65,8 +65,8 @@ export default class PreorderModal {
         var body = {
             'subscriber_data': {
                 'email': $('#preorder-email').val(),
-				'phone': $('#preorder-phone').val(),
-				'name': $('#preorder-name').val(),
+                'phone': $('#preorder-phone').val(),
+                'name': $('#preorder-name').val(),
                 'custom_fields': {
                     'product_name': $('#preorder-product').val()
                 }
