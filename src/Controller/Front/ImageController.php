2991f2c6ed0a3344df38514bffb4afd2873b8fc8
Lukáš Kielar <lukas.kielar@shopsys.com>
2022-02-15 15:46:31 +0100

    Redirect product images from ID to product name

--- src/Controller/Front/ImageController.php
+++ src/Controller/Front/ImageController.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Controller\Front;
 
+use App\Component\FileUpload\FileNamingConvention;
+use App\Component\Image\ImageRepository;
+use App\Model\Product\ProductFacade;
 use League\Flysystem\FilesystemInterface;
 use Shopsys\FrameworkBundle\Component\Image\Config\ImageConfig;
 use Shopsys\FrameworkBundle\Component\Image\Exception\ImageException;
 use Shopsys\FrameworkBundle\Component\Image\Processing\ImageGeneratorFacade;
+use Shopsys\FrameworkBundle\Component\String\TransformString;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ImageController extends FrontBaseController
@@ -22,29 +26,66 @@ class ImageController extends FrontBaseController
      */
     private $filesystem;
 
+    private ProductFacade $productFacade;
+
+    private ImageRepository $imageRepository;
+
     /**
      * @param \App\Component\Image\Processing\ImageGeneratorFacade $imageGeneratorFacade
      * @param \League\Flysystem\FilesystemInterface $filesystem
+     * @param \App\Component\Image\ImageRepository $imageRepository
+     * @param \App\Model\Product\ProductFacade $productFacade
      */
-    public function __construct(ImageGeneratorFacade $imageGeneratorFacade, FilesystemInterface $filesystem)
-    {
+    public function __construct(
+        ImageGeneratorFacade $imageGeneratorFacade,
+        FilesystemInterface $filesystem,
+        ImageRepository $imageRepository,
+        ProductFacade $productFacade
+    ) {
         $this->imageGeneratorFacade = $imageGeneratorFacade;
         $this->filesystem = $filesystem;
+        $this->productFacade = $productFacade;
+        $this->imageRepository = $imageRepository;
     }
 
     /**
-     * @param mixed $entityName
-     * @param mixed $type
-     * @param mixed $sizeName
-     * @param mixed $imageId
+     * @param string $entityName
+     * @param string|null $type
+     * @param string|null $sizeName
+     * @param int|string $imageId
+     * @param string $extension
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\StreamedResponse
      */
-    public function getImageAction($entityName, $type, $sizeName, $imageId)
+    public function getImageAction($entityName, $type, $sizeName, $imageId, $extension)
     {
         if ($sizeName === ImageConfig::DEFAULT_SIZE_NAME) {
             $sizeName = null;
         }
 
         try {
+            if ($entityName === FileNamingConvention::PRODUCT_CLASS_NAME && is_numeric($imageId)) {
+                $image = $this->imageRepository->getById($imageId);
+                $product = $this->productFacade->getById($image->getEntityId());
+                $newImageId = TransformString::stringToFriendlyUrlSlug($product->getName()) . '_' . $imageId;
+
+                if ($type === null) {
+                    return $this->redirectToRoute('front_image_without_type', [
+                        'entityName' => $entityName,
+                        'sizeName' => $sizeName,
+                        'imageId' => $newImageId,
+                        'extension' => $extension,
+                    ]);
+                } else {
+                    return $this->redirectToRoute('front_image', [
+                        'entityName' => $entityName,
+                        'type' => $type,
+                        'sizeName' => $sizeName,
+                        'imageId' => $newImageId,
+                        'extension' => $extension,
+                    ]);
+                }
+            }
+
             $imageFilepath = $this->imageGeneratorFacade->generateImageAndGetFilepath($entityName, $imageId, $type, $sizeName);
         } catch (ImageException $e) {
             $message = sprintf(
